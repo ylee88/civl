6b519737409478ac4458e7c12795f638af1fb496
Tim Zirkel <zirkeltk@udel.edu>
2013-01-09 14:28:45 +0000

    Created CIVL Internal Exception.  Removed unused util components.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@28 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/util/CIVLInternalException.java
+++ src/edu/udel/cis/vsl/civl/util/CIVLInternalException.java
@@ -1,21 +1,21 @@
-package edu.udel.cis.vsl.sarl.util;
+package edu.udel.cis.vsl.civl.util;
 
 /**
- * A SARL internal exception represents an error that is
+ * A CIVL internal exception represents an error that is
  * "not supposed to happen." It can be used like an assertion, whenever you feel
  * that something should always be true. It is a runtime exception, so there is
  * no need to declare or catch it. It will be thrown all the way up to main and
  * reported.
  */
-public class SARLInternalException extends RuntimeException {
+public class CIVLInternalException extends RuntimeException {
 
 	/**
 	 * The generated serial ID to implement Serializaeable.
 	 */
 	private static final long serialVersionUID = 6522079858283496490L;
 
-	public SARLInternalException(String s) {
-		super("A SARL internal error has occurred.\n"
+	public CIVLInternalException(String s) {
+		super("A CIVL internal error has occurred.\n"
 				+ "Please send an error report to siegel@udel.edu.\n" + s);
 	}
 
