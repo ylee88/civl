0596330f96bb2f45bf2377de1a4787c1f0a4c9d9
Stephen Siegel <siegel@udel.edu>
2013-07-16 01:35:08 +0000

    Started working on CIVL library but found lots of things that need fixing in semantics module
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@135 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/library/civlc/CivlcExecutor.java
+++ src/edu/udel/cis/vsl/civl/library/civlc/CivlcExecutor.java
@@ -7,11 +7,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 import edu.udel.cis.vsl.civl.model.IF.Identifier;
+import edu.udel.cis.vsl.civl.model.IF.expression.Expression;
 import edu.udel.cis.vsl.civl.model.IF.statement.CallStatement;
 import edu.udel.cis.vsl.civl.model.IF.statement.Statement;
+import edu.udel.cis.vsl.civl.model.IF.type.Type;
 import edu.udel.cis.vsl.civl.semantics.Executor;
 import edu.udel.cis.vsl.civl.semantics.IF.LibraryExecutor;
 import edu.udel.cis.vsl.civl.state.State;
+import edu.udel.cis.vsl.civl.util.CIVLInternalException;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
 import edu.udel.cis.vsl.sarl.IF.object.StringObject;
 
@@ -37,7 +41,77 @@ public class CivlcExecutor implements LibraryExecutor {
 	 */
 	@Override
 	public String name() {
-		return "civl";
+		return "civlc";
+	}
+
+	/**
+	 * Returns the type of the elements being malloc'ed. For example, in the
+	 * statement "p=(int*)malloc(n*sizeof(int))", the type returned would be
+	 * int.
+	 * 
+	 * @param mallocStatement
+	 *            an invocation of malloc
+	 * @return the type of the elements being malloc'ed
+	 */
+	private Type getMallocType(CallStatement mallocStatement) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Arguments to malloc are: (0) pointer to heap, (1) integer size. It
+	 * returns a pointer to the newly allocated object.
+	 * 
+	 * Arugment should have from n*sizeof(t) where n is some integer expression
+	 * and t is a type. Furthermore, it should be cast to that type. There is
+	 * some inference here.
+	 * 
+	 * The value of the heap should be the heapType, a symbolic type
+	 * array-of-(union_i(array-of-t_i)), for some types t_1,t_2,....
+	 * 
+	 * Since the size argument has been evaluated, search for SIZEOF ? Or should
+	 * this have already been done and stuck in the malloc?
+	 * 
+	 * Look at cast outside of malloc.
+	 * 
+	 * Other idea: create a new symbolic value "object(size)". Can be an
+	 * uninterpreted function. Or struct. It doesn't matter. It can have an
+	 * offset reference into it. Malloc returns the offset reference with offset
+	 * 0. When that reference is cast to something else, actually change the
+	 * value----there is a cascading cast which casts the object(size) to
+	 * int[10] or whatever. Hence the cast does have a side effect on the state?
+	 * 
+	 * @param state
+	 *            the pre-state
+	 * @param pid
+	 *            PID of process excecuting malloc
+	 * @param type
+	 *            of elements being malloc'ed
+	 * @param lhs
+	 *            left-hand side of assignment; could be null
+	 * @param heapReference
+	 *            reference to heap being used to perform malloc
+	 * @param size
+	 *            number of bytes being malloc'ed
+	 * @return the post-state
+	 */
+	private State executeMalloc(State state, int pid, Type type,
+			Expression lhs, SymbolicExpression heapReference,
+			NumericExpression size) {
+		// TODO
+		// get the dynamicType from the type
+		// count is size/sizeof(dynamicType)
+		// create a new symbolic constant X (?) of type
+		// array-of-dynamicType-of-length-count
+		// let oldHeap be the value obtained by dereferencing
+		// heapReference in state
+		// let newHeap be the result of appending to oldHeap
+		// the injection of X into the union type
+		// assign newHeap to heapReference
+		// create reference to element 0 of array?
+		// how to create reference to injection?
+
+		return null;
 	}
 
 	/*
@@ -53,17 +127,48 @@ public class CivlcExecutor implements LibraryExecutor {
 		Identifier name;
 		State result = null;
 		SymbolicExpression[] arguments;
+		CallStatement call;
+		Expression lhs;
 
 		if (!(statement instanceof CallStatement)) {
-			throw new RuntimeException("Unsupported statement for stdlib: "
+			throw new CIVLInternalException("Unsupported statement for civlc: "
 					+ statement);
 		}
-		name = ((CallStatement) statement).function().name();
+		call = (CallStatement) statement;
+		name = call.function().name();
+		lhs = call.lhs();
 		arguments = new SymbolicExpression[((CallStatement) statement)
 				.arguments().size()];
 		for (int i = 0; i < ((CallStatement) statement).arguments().size(); i++) {
 			arguments[i] = primaryExecutor.evaluator().evaluate(state, pid,
-					((CallStatement) statement).arguments().elementAt(i));
+					call.arguments().elementAt(i));
+		}
+		switch (name.name()) {
+		case "$malloc":
+			result = executeMalloc(state, pid, getMallocType(call), lhs,
+					arguments[0], (NumericExpression) arguments[1]);
+			break;
+		case "$free":
+			// put in stdio? case "printf":
+		case "$memcpy":
+		case "$message_pack":
+		case "$message_source":
+		case "$message_tag":
+		case "$message_dest":
+		case "$message_size":
+		case "$message_unpack":
+		case "$comm_create":
+		case "$comm_destroy":
+		case "$comm_nprocs":
+		case "$comm_enqueue":
+		case "$comm_probe":
+		case "$comm_seek":
+		case "$comm_dequeue":
+		case "$comm_chan_size":
+		case "$comm_total_size":
+		default:
+			throw new CIVLInternalException("Unknown civlc function: " + name
+					+ "\n" + statement);
 		}
 		if (name.name().equals("malloc")) {
 			assert arguments.length == 2;
