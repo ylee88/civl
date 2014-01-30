97355c5a4e2c0a7e49ce6707435eebeed9ef4711
Stephen Siegel <siegel@udel.edu>
2014-01-30 02:55:06 +0000

    Corrected parameter in benchmark, which was higher than what the comment said it should be.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@546 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- bench/edu/udel/cis/vsl/civl/BarrierBenchmark.java
+++ bench/edu/udel/cis/vsl/civl/BarrierBenchmark.java
@@ -14,7 +14,7 @@ public class BarrierBenchmark {
 
 	public static void main(String[] args) {
 		// -inputB=7: 26 seconds
-		ui.run("verify -inputB=8 examples/concurrency/barrier.cvl");
+		ui.run("verify -inputB=7 examples/concurrency/barrier.cvl");
 	}
 
 }
