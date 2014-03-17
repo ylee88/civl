4e47f0d7569a1698f7c1850558344d6cb834b6ff
Manchun Zheng <zmanchun@gmail.com>
2014-03-17 17:36:24 +0000

    updated date for v0.9.1
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@781 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/CIVL.java
+++ src/edu/udel/cis/vsl/civl/CIVL.java
@@ -22,13 +22,13 @@ public class CIVL {
 	}
 
 	/** The version of this release of CIVL. */
-	public final static String version = "0.9";
+	public final static String version = "0.9.1";
 
 	/**
 	 * The date of this release of CIVL. Format: YYYY-MM-DD in accordance with
 	 * ISO 8601.
 	 */
-	public final static String date = "2014-03-14";
+	public final static String date = "2014-03-17";
 
 	/**
 	 * Execute the appropriate CIVL tool(s) based on the command line arguments.
