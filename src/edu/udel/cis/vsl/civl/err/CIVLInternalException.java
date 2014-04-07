3589cc758a6e4ae191f6b97ab642cdbfed707a32
Manchun Zheng <zmanchun@gmail.com>
2014-04-07 14:39:38 +0000

    add CIVL trac system for error report.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@878 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/err/CIVLInternalException.java
+++ src/edu/udel/cis/vsl/civl/err/CIVLInternalException.java
@@ -18,8 +18,10 @@ public class CIVLInternalException extends CIVLException {
 	private static final long serialVersionUID = 6522079858283496490L;
 
 	public CIVLInternalException(String s, CIVLSource source) {
-		super("A CIVL internal error has occurred.\n"
-				+ "Please send an error report to civl-dev@udel.edu.\n" + s,
+		super(
+				"A CIVL internal error has occurred.\n"
+						+ "Please report the error at CIVL Trac (https://vsl.cis.udel.edu/trac/civl)"
+						+ " or send an email to civl-dev@udel.edu.\n" + s,
 				source);
 	}
 
