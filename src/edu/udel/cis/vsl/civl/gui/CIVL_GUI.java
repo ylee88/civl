61ea360fd9bd8cb98aaa4d6ee15fae41b82ebc5a
Ben Handanyan <benhandanyan@users.noreply.github.com>
2014-05-01 18:18:30 +0000

    GUI updates
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@955 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/gui/CIVL_GUI.java
+++ src/edu/udel/cis/vsl/civl/gui/CIVL_GUI.java
@@ -1,13 +1,17 @@
 package edu.udel.cis.vsl.civl.gui;
 
 import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeSelectionModel;
 
+import edu.udel.cis.vsl.civl.model.IF.statement.Statement;
 import edu.udel.cis.vsl.civl.state.IF.ProcessState;
 import edu.udel.cis.vsl.civl.state.IF.StackEntry;
 import edu.udel.cis.vsl.civl.state.IF.State;
@@ -16,17 +20,18 @@ import edu.udel.cis.vsl.civl.transition.CompoundTransition;
 import edu.udel.cis.vsl.civl.transition.Step;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
 
-public class CIVL_GUI extends JFrame {
-	/**
-	 * 
-	 */
+public class CIVL_GUI extends JFrame implements TreeSelectionListener {
 	private static final long serialVersionUID = 1L;
+	private final int TRANSITION_NODE = 0;
+	private final int STATE_NODE = 1;
+	private final int STATEMENT_NODE = 2;
 	private JTree stateTree;
 	private JTree transitionTree;
+	private JTree singleTransTree;
 	private JScrollPane transitionView;
 	private JScrollPane stateView;
 	private JSplitPane split;
-	protected CompoundTransition[] transitions;
+	private CompoundTransition[] transitions;
 
 	/**
 	 * Constructor for the CIVL GUI
@@ -58,7 +63,7 @@ public class CIVL_GUI extends JFrame {
 		// draw the transitions
 		transitionView = drawTransitions();
 		// draw the state
-		stateView = drawState(transitions[0].getStep(0).start());
+		stateView = drawSingleTransition(transitions[0],0);
 
 		// Add the views to the panels
 		split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, transitionView,
@@ -94,11 +99,6 @@ public class CIVL_GUI extends JFrame {
 		}
 
 		for (int i = 0; i < dyscopes.length; i++) {
-			int parentID = state.getParentId(i);
-
-			if (parentID != -1) {
-				treeNodes[parentID].add(treeNodes[i]);
-			}
 
 			DefaultMutableTreeNode variables = new DefaultMutableTreeNode(
 					"Variables");
@@ -111,6 +111,12 @@ public class CIVL_GUI extends JFrame {
 				DefaultMutableTreeNode variableNode = new DefaultMutableTreeNode(
 						variableName + " = " + s.toString());
 				variables.add(variableNode);
+			}
+
+			int parentID = state.getParentId(i);
+
+			if (parentID != -1) {
+				treeNodes[parentID].add(treeNodes[i]);
 			}
 
 		}
@@ -136,6 +142,9 @@ public class CIVL_GUI extends JFrame {
 		stateTree = new JTree(top);
 		stateTree.getSelectionModel().setSelectionMode(
 				TreeSelectionModel.SINGLE_TREE_SELECTION);
+		for (int i = 0; i < stateTree.getRowCount(); i++) {
+			stateTree.expandRow(i);
+		}
 
 		// Create the view of the tree and set their preferred size
 		JScrollPane treeView = new JScrollPane(stateTree);
@@ -149,20 +158,35 @@ public class CIVL_GUI extends JFrame {
 	 * @return JScrollPane
 	 */
 	public JScrollPane drawTransitions() {
-		
+
 		DefaultMutableTreeNode top = new DefaultMutableTreeNode("Transitions");
-		for(int i = 0; i < transitions.length;i++) {
-			DefaultMutableTreeNode tNode = new DefaultMutableTreeNode("Transition: " + i);
-			for(Step s : transitions[i].getSteps()) {
-				tNode.add(new DefaultMutableTreeNode(s.toString()));
+		for (int i = 0; i < transitions.length; i++) {
+			TransitionNode transitionNode = new TransitionNode("Transition: "
+					+ i + ", Process: p" + transitions[i].processIdentifier(),
+					transitions[i]);
+			int index = 0;
+			for (Step s : transitions[i].getSteps()) {
+				DefaultMutableTreeNode stepNode = new DefaultMutableTreeNode(
+						"Step " + index + ": " + s.toString());
+				transitionNode.add(stepNode);
+				StateNode startNode = new StateNode("Starting State: "
+						+ s.start().toString(), s.start());
+				StateNode targetNode = new StateNode("Target State: "
+						+ s.target().toString(), s.target());
+				StatementNode statementNode = new StatementNode("Statement: "
+						+ s.statement().toString(), s.statement());
+				stepNode.add(startNode);
+				stepNode.add(targetNode);
+				stepNode.add(statementNode);
+				index++;
 			}
-			//+ " - " + s.statement().toString() + " -> " + new stateButton(s.target()))
-			top.add(tNode);
+			top.add(transitionNode);
 		}
 		// Create a tree that allows one selection at a time.
 		transitionTree = new JTree(top);
 		transitionTree.getSelectionModel().setSelectionMode(
 				TreeSelectionModel.SINGLE_TREE_SELECTION);
+		transitionTree.addTreeSelectionListener(this);
 
 		// Create the view of the tree and set their preferred size
 		JScrollPane transitionView = new JScrollPane(transitionTree);
@@ -170,27 +194,132 @@ public class CIVL_GUI extends JFrame {
 		return transitionView;
 	}
 
-//	@Override
-//	public void actionPerformed(ActionEvent e) {
-//		split.remove(stateView);
-//		stateView = drawState(((stateButton) e.getSource()).getState());
-//		split.add(stateView);
-//
-//	}
-//
-//	@SuppressWarnings("serial")
-//	private class stateButton extends JButton {
-//		State state;
-//
-//		public stateButton(State state) {
-//			super("State: " + state.getCanonicId());
-//			this.state = state;
-//
-//		}
-//
-//		public State getState() {
-//			return state;
-//		}
-//	}
+	public JScrollPane drawSingleTransition(CompoundTransition t, int num) {
+		TransitionNode top = new TransitionNode("Transition: " + num
+				+ ", Process: p" + t.processIdentifier(), t);
+		int index = 0;
+		for (Step s : t.getSteps()) {
+			DefaultMutableTreeNode stepNode = new DefaultMutableTreeNode(
+					"Step = " + index + ": " + s.toString());
+			top.add(stepNode);
+			StateNode startNode = new StateNode("Starting State: "
+					+ s.start().toString(), s.start());
+			StateNode targetNode = new StateNode("Target State: "
+					+ s.target().toString(), s.target());
+			StatementNode statementNode = new StatementNode("Statement: "
+					+ s.statement().toString(), s.statement());
+			stepNode.add(startNode);
+			stepNode.add(targetNode);
+			stepNode.add(statementNode);
+			index++;
+		}
+		// Create a tree that allows one selection at a time.
+		singleTransTree = new JTree(top);
+		singleTransTree.getSelectionModel().setSelectionMode(
+				TreeSelectionModel.SINGLE_TREE_SELECTION);
 
+		// Create the view of the tree and set their preferred size
+		JScrollPane transitionView = new JScrollPane(singleTransTree);
+		transitionView.setPreferredSize(new Dimension(600, 500));
+		return transitionView;
+
+	}
+
+	private abstract class GUINODE extends DefaultMutableTreeNode {
+		private static final long serialVersionUID = 1L;
+
+		public GUINODE(String name) {
+			super(name);
+		}
+
+		public abstract int getID();
+
+	}
+
+	private class TransitionNode extends GUINODE {
+		private static final long serialVersionUID = 1L;
+		public final int id = TRANSITION_NODE;
+		protected CompoundTransition transition;
+
+		public TransitionNode(String name, CompoundTransition t) {
+			super(name);
+			transition = t;
+		}
+
+		public CompoundTransition getTransition() {
+			return transition;
+		}
+
+		public int getID() {
+			return id;
+		}
+	}
+
+	private class StateNode extends GUINODE {
+		private static final long serialVersionUID = 1L;
+		public final int id = STATE_NODE;
+		protected State state;
+
+		public StateNode(String name, State s) {
+			super(name);
+			state = s;
+
+		}
+
+		public State getState() {
+			return state;
+		}
+
+		public int getID() {
+			return id;
+		}
+	}
+
+	private class StatementNode extends GUINODE {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		public final int id = STATEMENT_NODE;
+		protected Statement statement;
+
+		public StatementNode(String name, Statement s) {
+			super(name);
+			statement = s;
+		}
+
+		@SuppressWarnings("unused")
+		public Statement getStatement() {
+			return statement;
+		}
+
+		public int getID() {
+			return id;
+		}
+	}
+
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+		try {
+			TransitionNode n = (TransitionNode) transitionTree
+					.getLastSelectedPathComponent();
+			if (n == null)
+				return;
+			split.remove(split.getRightComponent());
+			transitionView = drawSingleTransition(n.getTransition(), 1);
+			split.setRightComponent(transitionView);
+		} catch (Exception e1) {
+			try {
+				StateNode n = (StateNode) transitionTree
+						.getLastSelectedPathComponent();
+				if (n == null)
+					return;
+				split.remove(split.getRightComponent());
+				stateView = drawState(n.getState());
+				split.setRightComponent(stateView);
+			} catch (Exception e2) {
+				return;
+			}
+		}
+	}
 }
