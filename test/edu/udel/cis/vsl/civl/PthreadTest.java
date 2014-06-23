a853a2a18cd0ba677615a58e88361e38fdc74d26
John Edenhofner <johneden@udel.edu>
2014-06-23 17:06:26 +0000

    Created a JUNIT PthreadTest
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1162 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ test/edu/udel/cis/vsl/civl/PthreadTest.java
@@ -0,0 +1,35 @@
+package edu.udel.cis.vsl.civl;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import edu.udel.cis.vsl.abc.err.IF.ABCException;
+import edu.udel.cis.vsl.civl.run.IF.UserInterface;
+
+public class PthreadTest {
+	/* *************************** Static Fields *************************** */
+
+	private static File rootDir = new File(new File("examples"),
+			"translation/pthread");
+
+	private static UserInterface ui = new UserInterface();
+
+	/* *************************** Helper Methods *************************** */
+
+	private static String filename(String name) {
+		return new File(rootDir, name).getPath();
+	}
+
+	/* **************************** Test Methods *************************** */
+
+	@Test
+	public void pointerProb() throws ABCException {
+		assertTrue(ui.run("verify", filename("pointerProb.c")));
+	}
+
+}
