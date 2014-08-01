0bbae8b72e73483941cc5ae8d3e1ad5b0baadaa1
Steve Noyes <noyes@udel.edu>
2014-08-01 18:21:09 +0000

    added more features to GUI
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1366 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/gui/common/NewRunConfigGUI.java
+++ src/edu/udel/cis/vsl/civl/gui/common/NewRunConfigGUI.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -50,12 +51,6 @@ import edu.udel.cis.vsl.gmc.Option;
 public class NewRunConfigGUI extends JFrame {
 
 	private static final long serialVersionUID = 5152675076717228871L;
-	// ***TEMPORARY********************
-	private JButton bt_revert1;
-	private JButton bt_revert2;
-	private JButton bt_apply1;
-	private JButton bt_apply2;
-	// ***TEMPORARY/********************
 
 	/**
 	 * The views for each command represented as a card layout in this JPanel.
@@ -76,6 +71,11 @@ public class NewRunConfigGUI extends JFrame {
 	 * The panel that contains the JTree of commands.
 	 */
 	private JPanel p_commands;
+	
+	/**
+	 * A simple container for GUI organization.
+	 */
+	private JPanel p_container;
 
 	/**
 	 * The JTextArea that contains the header text.
@@ -91,6 +91,11 @@ public class NewRunConfigGUI extends JFrame {
 	 * The CIVL logo.
 	 */
 	private JLabel lb_icon;
+	
+	/**
+	 * A simple label. (Name: )
+	 */
+	private JLabel lb_Name;
 
 	/**
 	 * The label: "Chosen File". It indicates the JTextField that displays the
@@ -145,6 +150,21 @@ public class NewRunConfigGUI extends JFrame {
 	 * Runs the selected configuration.
 	 */
 	private JButton bt_run;
+	
+	/**
+	 * Creates a new run configuration.
+	 */
+	private JButton bt_new;
+	
+	/**
+	 * Duplicates the currently selected run configuration.
+	 */
+	private JButton bt_duplicate;
+	
+	/**
+	 * Deletes the currently selected run configuration.
+	 */
+	private JButton bt_deleteConfig;
 
 	/**
 	 * Allows the user to browse for a file.
@@ -542,7 +562,7 @@ public class NewRunConfigGUI extends JFrame {
 		viewCards.setBorder(new TitledBorder(null, null, TitledBorder.LEADING,
 				TitledBorder.TOP, null, null));
 		// viewCards.setBounds(227, 65, 967, 568);
-		viewCards.setBounds(227, 99, 967, 534);
+		viewCards.setBounds(0, 34, 967, 488);
 		viewCards.setLayout(viewCardsLayout);
 
 		viewCards.add(commands[0].getView(), "help");
@@ -559,39 +579,49 @@ public class NewRunConfigGUI extends JFrame {
 	public void initJPanel() {
 		p_commands = new JPanel();
 		p_header = new JPanel();
-
+		p_container = new JPanel();
+		p_container.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		p_commands.setBorder(new TitledBorder(null, "Commands",
 				TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		p_commands.setBounds(10, 65, 205, 607);
+		
 		p_commands.setLayout(null);
-
-		p_header.setBounds(0, 0, 1200, 53);
+		p_container.setLayout(null);
 		p_header.setLayout(null);
-
+		
+		p_container.setBounds(227, 65, 967, 566);
+		p_commands.setBounds(10, 99, 205, 573);
+		p_header.setBounds(0, 0, 1200, 53);
+		
+		
 		initCommands();
 		initJTable();
 		initCards();
 		initJTree();
-
+				
+		getContentPane().add(p_container);
 		getContentPane().add(p_header);
 		getContentPane().add(p_commands);
-		getContentPane().add(viewCards);
+		
 		getContentPane().add(bt_cancel);
-		getContentPane().add(bt_run);
+		getContentPane().add(bt_run);		
+		getContentPane().add(bt_new);
+		getContentPane().add(bt_duplicate);
+		getContentPane().add(bt_deleteConfig);
 		p_header.add(lb_icon);
 		p_commands.add(jt_commands);
 		p_header.add(ta_header_info);
 		p_header.add(ta_header);
+		p_container.add(viewCards);
+		p_container.add(bt_apply);
+		p_container.add(bt_revert);
+		p_container.add(tf_name);
+		p_container.add(lb_Name);
 	}
 
 	/**
 	 * Creates all of the JButtons in the GUI.
 	 */
 	public void initJButton() {
-		bt_revert1 = new JButton("Revert");
-		bt_revert2 = new JButton("Revert");
-		bt_apply1 = new JButton("Apply");
-		bt_apply2 = new JButton("Apply");
 		bt_cancel = new JButton("Cancel");
 		bt_run = new JButton("Run");
 		bt_browse_pa = new JButton("Browse...");
@@ -601,22 +631,24 @@ public class NewRunConfigGUI extends JFrame {
 		bt_browse_vf = new JButton("Browse...");
 		bt_revert = new JButton("Reset");
 		bt_apply = new JButton("Apply");
+		bt_new = new JButton("New");
+		bt_duplicate = new JButton("Duplicate");
+		bt_deleteConfig = new JButton("X");
 
-		bt_apply.setBounds(694, 453, 117, 29);
-		bt_revert.setBounds(823, 453, 117, 29);
+		bt_apply.setBounds(717, 531, 117, 29);
+		bt_revert.setBounds(844, 531, 117, 29);
 
-		bt_revert1.setBounds(823, 487, 117, 29);
-		bt_revert2.setBounds(823, 487, 117, 29);
-		bt_apply1.setBounds(694, 487, 117, 29);
-		bt_apply2.setBounds(694, 487, 117, 29);
 		bt_cancel.setBounds(1077, 643, 117, 29);
 		bt_run.setBounds(948, 643, 117, 29);
 		bt_browse_pa.setBounds(823, 22, 117, 29);
 		bt_browse_pp.setBounds(823, 22, 117, 29);
 		bt_browse_rp.setBounds(823, 22, 117, 29);
 		bt_browse_ru.setBounds(823, 22, 117, 29);
-		bt_browse_vf.setBounds(823, 22, 117, 29);
-
+		bt_browse_vf.setBounds(823, 22, 117, 29);		
+		bt_new.setBounds(10, 65, 53, 29);		
+		bt_duplicate.setBounds(59, 65, 89, 29);		
+		bt_deleteConfig.setBounds(142, 65, 61, 29);
+		
 		ActionListener browse = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String examplesPath = "/Users/noyes/Documents/workspace/CIVL/examples";
@@ -662,16 +694,17 @@ public class NewRunConfigGUI extends JFrame {
 	}
 
 	// TODO: FILL TABLE WITH "ALLOWED" OPTIONS INSTEAD OF ALL OF THEM
+	// TODO: MAKE COLUMNS SMALLER, SO IT DOESN'T LOOK STUPID
 	public void initJTable() {
 		sp_optTable_ru = new JScrollPane();
 		sp_optTable_vf = new JScrollPane();
 		sp_inputTable_ru = new JScrollPane();
 		sp_inputTable_vf = new JScrollPane();
 
-		sp_optTable_ru.setBounds(6, 6, 967 - 36, 450);
-		sp_optTable_vf.setBounds(6, 6, 967 - 36, 450);
-		sp_inputTable_ru.setBounds(6, 6, 967 - 36, 450);
-		sp_inputTable_vf.setBounds(6, 6, 967 - 36, 450);
+		sp_optTable_ru.setBounds(6, 6, 967 - 36, 425);
+		sp_optTable_vf.setBounds(6, 6, 967 - 36, 425);
+		sp_inputTable_ru.setBounds(6, 6, 967 - 36, 425);
+		sp_inputTable_vf.setBounds(6, 6, 967 - 36, 425);
 
 		tbl_optTable_ru = new CIVLTable(new int[] { 1, 2 });
 		tbl_optTable_vf = new CIVLTable(new int[] { 1, 2 });
@@ -787,9 +820,9 @@ public class NewRunConfigGUI extends JFrame {
 		lb_chosenFile_rp = new JLabel("Chosen File:");
 		lb_chosenFile_ru = new JLabel("Chosen File:");
 		lb_chosenFile_vf = new JLabel("Chosen File:");
-		JLabel lb_Name = new JLabel("Name:");
+		lb_Name = new JLabel("Name:");
 
-		lb_Name.setBounds(240, 71, 61, 16);
+		lb_Name.setBounds(9, 8, 61, 16);
 		lb_chosenFile_pa.setBounds(6, 6, 100, 16);
 		lb_chosenFile_pp.setBounds(6, 6, 100, 16);
 		lb_chosenFile_rp.setBounds(6, 6, 100, 16);
@@ -798,7 +831,6 @@ public class NewRunConfigGUI extends JFrame {
 		lb_icon.setBounds(1040, 3, 207, 47);
 		lb_icon.setIcon(new ImageIcon("Images/logo.png"));
 
-		getContentPane().add(lb_Name);
 	}
 
 	/**
@@ -836,9 +868,9 @@ public class NewRunConfigGUI extends JFrame {
 		tf_chosenFile_rp.setBounds(6, 21, 805, 28);
 		tf_chosenFile_ru.setBounds(6, 21, 805, 28);
 		tf_chosenFile_vf.setBounds(6, 21, 805, 28);
-		tf_name.setBounds(289, 65, 905, 28);
+		tf_name.setBounds(52, 4, 905, 28);
 
-		getContentPane().add(tf_name);
+		
 	}
 
 	/**
