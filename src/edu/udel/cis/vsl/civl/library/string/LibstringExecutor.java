48787d9be0c93042e21862221db38cf21bc38142
John Edenhofner <johneden@udel.edu>
2014-08-08 22:12:26 +0000

    Implemented strcmp as a system function and added usage to string.cvl for testing
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1401 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/library/string/LibstringExecutor.java
+++ src/edu/udel/cis/vsl/civl/library/string/LibstringExecutor.java
@@ -3,6 +3,8 @@
  */
 package edu.udel.cis.vsl.civl.library.string;
 
+import java.util.Arrays;
+
 import edu.udel.cis.vsl.civl.config.IF.CIVLConfiguration;
 import edu.udel.cis.vsl.civl.dynamic.IF.SymbolicUtility;
 import edu.udel.cis.vsl.civl.library.common.BaseLibraryExecutor;
@@ -23,10 +25,12 @@ import edu.udel.cis.vsl.civl.state.IF.UnsatisfiablePathConditionException;
 import edu.udel.cis.vsl.sarl.IF.expr.ArrayElementReference;
 import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.ReferenceExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression.SymbolicOperator;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicArrayType;
 import edu.udel.cis.vsl.sarl.collections.IF.SymbolicSequence;
+
 
 /**
  * Executor for stdlib function calls.
@@ -37,6 +41,10 @@ import edu.udel.cis.vsl.sarl.collections.IF.SymbolicSequence;
  */
 public class LibstringExecutor extends BaseLibraryExecutor implements
 		LibraryExecutor {
+	
+	private SymbolicConstant initialContentsFunction;
+	
+	private SymbolicArrayType stringSymbolicType;
 
 	/* **************************** Constructors *************************** */
 
@@ -111,6 +119,11 @@ public class LibstringExecutor extends BaseLibraryExecutor implements
 			state = execute_strlen(state, pid, process, lhs, arguments,
 					argumentValues, call.getSource());
 			break;
+		case "strcmp":
+			state = execute_strcmp(state, pid, process, lhs, arguments,
+					argumentValues, call.getSource());
+			break;
+
 		default:
 			throw new CIVLInternalException("Unknown stdlib function: " + name,
 					call);
@@ -118,6 +131,7 @@ public class LibstringExecutor extends BaseLibraryExecutor implements
 		state = stateFactory.setLocation(state, pid, call.target());
 		return state;
 	}
+
 
 	private State execute_strcpy(State state, int pid, String process,
 			LHSExpression lhs, Expression[] arguments,
@@ -177,6 +191,102 @@ public class LibstringExecutor extends BaseLibraryExecutor implements
 		if (lhs != null)
 			state = primaryExecutor.assign(state, pid, process, lhs,
 					argumentValues[0]);
+		return state;
+	}
+	
+	private State execute_strcmp(State state, int pid, String process,
+			LHSExpression lhs, Expression[] arguments,
+			SymbolicExpression[] argumentValues, CIVLSource source)
+			throws UnsatisfiablePathConditionException {
+		Evaluation eval;
+		int startIndex1;
+		int startIndex2;
+		int output;
+		SymbolicExpression charPointer1 = argumentValues[0];
+		SymbolicExpression charPointer2 = argumentValues[1];
+		int numChars1;
+		int numChars2;
+		SymbolicSequence<?> originalArray1;
+		SymbolicSequence<?> originalArray2;
+		String s1 = "";
+		String s2 = "";
+		
+		if (charPointer1.operator() == SymbolicOperator.CONCRETE && charPointer2.operator() == SymbolicOperator.CONCRETE){
+			startIndex1 = symbolicUtil.getArrayIndex(source, charPointer1);
+			startIndex2 = symbolicUtil.getArrayIndex(source, charPointer2);
+		}
+		else{
+			stringSymbolicType = (SymbolicArrayType) universe.canonic(universe
+					.arrayType(universe.characterType()));
+			initialContentsFunction = (SymbolicConstant) universe.canonic(universe
+					.symbolicConstant(universe.stringObject("contents"), universe
+							.functionType(Arrays.asList(stringSymbolicType, stringSymbolicType),
+									universe.integerType())));
+			SymbolicExpression outValue = universe.apply(initialContentsFunction, Arrays.asList(charPointer1, charPointer2));
+			state = primaryExecutor.assign(state, pid, process, lhs,
+					outValue);
+			
+			return state;
+		}
+		
+		if (charPointer1.type() instanceof SymbolicArrayType) {
+			originalArray1 = (SymbolicSequence<?>) charPointer1.argument(0);
+		} else {
+			SymbolicExpression arrayPointer1 = symbolicUtil.parentPointer(
+					source, charPointer1);
+			ArrayElementReference arrayRef1 = (ArrayElementReference) symbolicUtil
+					.getSymRef(charPointer1);
+			NumericExpression arrayIndex1 = arrayRef1.getIndex();
+			eval = evaluator.dereference(source, state, process, arrayPointer1,
+					false);
+
+			state = eval.state;
+			originalArray1 = (SymbolicSequence<?>) eval.value.argument(0);
+			startIndex1 = symbolicUtil.extractInt(source, arrayIndex1);
+		}
+		numChars1 = originalArray1.size();
+		for (int i = 0; i < numChars1; i++) {
+			SymbolicExpression charExpr = originalArray1.get(i + startIndex1);
+			Character theChar = universe.extractCharacter(charExpr);
+			s1 = s1.concat(theChar.toString());
+
+			if (theChar == '\0')
+				break;
+		}
+		
+		if (charPointer2.type() instanceof SymbolicArrayType) {
+			originalArray2 = (SymbolicSequence<?>) charPointer2.argument(0);
+		} else {
+			SymbolicExpression arrayPointer2 = symbolicUtil.parentPointer(
+					source, charPointer2);
+			ArrayElementReference arrayRef2 = (ArrayElementReference) symbolicUtil
+					.getSymRef(charPointer2);
+			NumericExpression arrayIndex2 = arrayRef2.getIndex();
+			eval = evaluator.dereference(source, state, process, arrayPointer2,
+					false);
+
+			state = eval.state;
+			originalArray2 = (SymbolicSequence<?>) eval.value.argument(0);
+			startIndex2 = symbolicUtil.extractInt(source, arrayIndex2);
+		}
+		numChars2 = originalArray2.size();
+		for (int i = 0; i < numChars2; i++) {
+			SymbolicExpression charExpr = originalArray2.get(i + startIndex2);
+			Character theChar = universe.extractCharacter(charExpr);
+			s2 = s2.concat(theChar.toString());
+
+			if (theChar == '\0')
+				break;
+		}
+		
+		if(s1.equals(s2))
+			output = 0;
+		else
+			output = -1;
+		
+		if (lhs != null)
+			state = primaryExecutor.assign(state, pid, process, lhs,
+					universe.integer(output));
 		return state;
 	}
 
