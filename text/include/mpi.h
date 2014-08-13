c2a3f74eafd207c0df09b6fc50ca1be78145fc9e
Ziqing Luo <ziqing@udel.edu>
2014-08-13 15:35:58 +0000

    changed the implementation of MPI_Reduce
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1421 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- text/include/mpi.h
+++ text/include/mpi.h
@@ -5,37 +5,21 @@
 #include<string.h>
 
 typedef enum {
-    MPIX_NO_OP,
-    MPI_MAX, 
-    MPI_MIN,     
-    MPI_SUM,
-    MPI_PROD,
-    MPI_LAND,   
-    MPI_BAND,   
-    MPI_LOR,     
-    MPI_BOR,     
-    MPI_LXOR,    
-    MPI_BXOR,    
-    MPI_MINLOC,  
-    MPI_MAXLOC,  
-    MPI_REPLACE
+    MPIX_NO_OP = CIVL_NO_OP,
+    MPI_MAX = CIVL_MAX, 
+    MPI_MIN = CIVL_MIN,     
+    MPI_SUM = CIVL_SUM,
+    MPI_PROD = CIVL_PROD,
+    MPI_LAND = CIVL_LAND,   
+    MPI_BAND = CIVL_BAND,   
+    MPI_LOR = CIVL_LOR,     
+    MPI_BOR = CIVL_BOR,     
+    MPI_LXOR = CIVL_LXOR,    
+    MPI_BXOR = CIVL_BXOR,    
+    MPI_MINLOC = CIVL_MINLOC,  
+    MPI_MAXLOC = CIVL_MAXLOC,  
+    MPI_REPLACE = CIVL_REPLACE
 }MPI_Op;
-
-/* Temporary use */
-#define MPI_NO_OP   CIVL_NO_OP
-#define MPI_MAX     CIVL_MAX   
-#define MPI_MIN     CIVL_MIN    
-#define MPI_SUM     CIVL_SUM
-#define MPI_PROD    CIVL_PROD    
-#define MPI_LAND    CIVL_LAND   
-#define MPI_BAND    CIVL_BAND
-#define MPI_LOR     CIVL_LOR
-#define MPI_BOR     CIVL_BOR
-#define MPI_LXOR    CIVL_LXOR
-#define MPI_BXOR    CIVL_BXOR
-#define MPI_MINLOC  CIVL_MINLOC
-#define MPI_MAXLOC  CIVL_MAXLOC
-#define MPI_REPLACE CIVL_REPLACE
 
 #include<mpi-common.h>
 #include<mpi.cvl>
