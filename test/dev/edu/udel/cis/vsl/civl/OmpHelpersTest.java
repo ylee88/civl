0495dcdd4a61a6cf1dd69eb6a0c66dda8f0dfd21
Stephen Siegel <siegel@udel.edu>
2014-09-26 20:28:18 +0000

    Adding a test for the OpenMP helper functions of civlc-omp.cvh.   Currently there is a problem running it resulting in an infinite loop at an error point.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1573 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ test/dev/edu/udel/cis/vsl/civl/OmpHelpersTest.java
@@ -0,0 +1,31 @@
+package edu.udel.cis.vsl.civl;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import edu.udel.cis.vsl.civl.run.IF.UserInterface;
+
+public class OmpHelpersTest {
+
+	/* *************************** Static Fields *************************** */
+
+	private static File rootDir = new File(new File("examples"), "omp");
+
+	private static UserInterface ui = new UserInterface();
+
+	/* *************************** Helper Methods ************************** */
+
+	private static String filename(String name) {
+		return new File(rootDir, name).getPath();
+	}
+
+	/* **************************** Test Methods *************************** */
+
+	@Test
+	public void adder() {
+		assertTrue(ui.run("run", filename("omp_helpers1.cvl")));
+	}
+}
