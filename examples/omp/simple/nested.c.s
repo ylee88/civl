dfb1c640d143bc462b9a236d7419c2d8b37dc9d2
Matthew B. Dwyer <matthewbdwyer@users.noreply.github.com>
2014-10-27 20:19:55 +0000

    fixed bug in expected output
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1660 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/omp/simple/nested.c.s
+++ examples/omp/simple/nested.c.s
@@ -4,8 +4,6 @@ int main (int argc, char *argv[]){
     int sum = 0, localsum=0;
     int n = 10;
     int i;
-#pragma omp parallel shared(sum) private(i)
-#pragma omp for 
     for(i = 0; i < n; i++){
         localsum += i;
     }
