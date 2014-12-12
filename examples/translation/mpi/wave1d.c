f0f2c558bbd27ad2854e6d52e81c994df9394c21
Ziqing Luo <ziqing@udel.edu>
2014-12-12 16:02:47 +0000

    changed the wave1d.c example. add diffusion1d.c example
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1728 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/translation/mpi/wave1d.c
+++ examples/translation/mpi/wave1d.c
@@ -13,7 +13,7 @@
 #define SQR(x) ((x)*(x))
 #define OWNER(index) ((nprocs*(index+1)-1)/nx)
 
-/* MPI message tag */
+/* MPI message tags */
 #define FROMLEFT     1
 #define FROMRIGHT    2
 #define DATAPASS     3
@@ -21,26 +21,25 @@
 /* Input parameters */
 #ifdef _CIVL
 
-$input int NXB = 5;
+$input int NXB = 5;               /* upper bound on nx */
 $input int nx;                    /* number of discrete points including endpoints */
-$assume 2 <= nx && nx <= NXB;      /* setting bounds */
+$assume 2 <= nx && nx <= NXB;     /* setting bounds */
 $input double c;                  /* physical constant to do with string */
-$assume c > 0.0;
+$assume c > 0.0;       
 $input int NSTEPSB = 5;        
-$input int nsteps;                /*  number of iterations */
+$input int nsteps;                /* number of time steps */
 $assume 0 < nsteps && nsteps <= NSTEPSB;
-$input int wstep = 1;
+$input int wstep = 1;             /* number of time steps between printing */
 $input int _NPROCS_LOWER_BOUND = 1;
 $input int _NPROCS_UPPER_BOUND = 4;
-double oracle[nsteps][nx];       /* array stores the results of sequential run in every step */
-$input double u_init[nx];        /* arbitrary input data */
+double oracle[nsteps + 1][nx + 2];/* result of sequential run at every time step */
+$input double u_init[nx];         /* arbitrary initial position of string */
 
 #else
 
 int nx, height_init, width_init;
 int nsteps, wstep;
 double c;
-double * u_init;
 
 #endif
 
@@ -48,8 +47,8 @@ double * u_init;
 double *u_prev, *u_curr, *u_next;
 double k;
 int nprocs, nxl, rank;
-int first;                       /* the first index of cells in global array */
-int left, right;                 /* left neighbor and right neighbor */
+int first;                       /* global index of first cell owned by this process */
+int left, right;                 /* ranks of left neighbor and right neighbor */
 
 /* Returns the global index of the first cell owned
  * by the process with given rank */
@@ -57,8 +56,7 @@ int firstForProc(int rank) {
   return (rank*nx)/nprocs;      
 }
 
-/* Returns the number of cells 
-   the given process owns */
+/* Returns the number of cells the given process owns */
 int countForProc(int rank) {
   int a = firstForProc(rank);
   int b = firstForProc(rank + 1);
@@ -68,17 +66,19 @@ int countForProc(int rank) {
 
 #ifndef _CIVL
 
-/* Initialize data array for running in MPI */
-void init() {
+/* Some particular initial condition for testing only */
+void init(int first, int nxl) {
   int i;
   double e = exp(1.0);
 
-  for(i = 0; i < nx; i++) {
-    if(i == 1 || i >= width_init)
-      u_init[i] = 0.0;
+  for(i = 1; i < nxl+1; i++) {
+    int idx = first + i - 1;
+
+    if(idx == 1 || idx >= width_init)
+      u_prev[i] = 0.0;
     else
-      u_init[i] = height_init * e *
-	exp(-1.0/(1-SQR(2.0*(i-width_init/2.0)/width_init)));
+      u_prev[i] = height_init * e *
+	exp(-1.0/(1-SQR(2.0*(idx-width_init/2.0)/width_init)));
   }
 }
 
@@ -93,16 +93,18 @@ void update() {
     u_next[i] = 2.0*u_curr[i] - u_prev[i] +
       k*(u_curr[i+1] + u_curr[i-1] -2.0*u_curr[i]);
   }
-  //cycle pointers
+  //cycle pointers:
   tmp = u_prev;
   u_prev = u_curr;
   u_curr = u_next;
   u_next = tmp;
 }
 
-/* Initialization function, initializes all parameters and data array
-   process 0 is responsible run in sequential for computing data for
-   comparison */
+/* Initialization function, initializes all parameters and data array.
+ * In CIVL mode, process 0 runs the complete problem sequentially to
+ * create the oracle which will be compared with the results of the
+ * parallel run.
+ */
 void initialization() {
   int i, j;
 
@@ -114,59 +116,41 @@ void initialization() {
   width_init = 10;
   nsteps = 500;
   wstep = 5;
-  u_init = (double *)malloc(nx * sizeof(double));
-  init();
 
 #endif
 
-  printf("Wave1d with nx=%d, c=%f, nsteps=%d, wstep=%d\n", nx, c, nsteps, wstep);
   assert(nx >= 2);
   assert(c > 0);
   assert(nsteps >= 1);
   assert(wstep >= 1 && wstep <= nsteps);
   k = c * c;
 
+  printf("Wave1d with nx=%d, c=%f, nsteps=%d, wstep=%d\n", nx, c, nsteps, wstep);
+
 #ifdef _CIVL
 
   // If in CIVL verification mode and rank is 0,
   // do a sequential run and store result in "oracle" 
   // for comparison later
-  if(rank == 0) {
-    double *seq_u_curr, *seq_u_prev, *seq_u_next;
-    double * tmp;
- 
-    seq_u_prev = (double *)malloc((nx + 2) * sizeof(double));
-    assert(seq_u_prev);
-    seq_u_curr = (double *)malloc((nx + 2) * sizeof(double));
-    assert(seq_u_curr);
-    seq_u_next = (double *)malloc((nx + 2) * sizeof(double));
-    assert(seq_u_next);
-    //Initialize seq_u_curr and seq_u_prev
-    memcpy(&seq_u_curr[1], u_init, sizeof(double) * nx);
-    memcpy(&seq_u_prev[1], u_init, sizeof(double) * nx);
-    // run in sequential.
-    // wirte data in time 0.
-    for(i = 0; i < nx; i++) 
-      oracle[0][i] = seq_u_curr[i + 1];
-    for(i = 1; i < nsteps; i++){
-      // exchange between head cell and tail cell.
-      seq_u_curr[0] = seq_u_curr[nx];
-      seq_u_curr[nx+1] = seq_u_curr[1];
-      // update
-      for (j = 1; j < nx + 1; j++){
-	seq_u_next[j] = 2.0*seq_u_curr[j] - seq_u_prev[j] +
-	  k*(seq_u_curr[j+1] + seq_u_curr[j-1] -2.0*seq_u_curr[j]);
-      }    
-      tmp = seq_u_prev;
-      seq_u_prev = seq_u_curr;
-      seq_u_curr = seq_u_next;
-      seq_u_next = tmp;
-      for(j = 0; j < nx; j++)
-	oracle[i][j] = seq_u_curr[j + 1];
+  if(rank == 0) { 
+    // sets constant boundaries for first string and
+    // it's virtual previous string
+    oracle[0][0] = 0;
+    oracle[0][nx + 1] = 0;
+    // initialization for first string and
+    // it's virtual previous string
+    for(i = 1; i < nx + 1; i++) {
+      oracle[0][i] = u_init[i - 1];
+      oracle[1][i] = u_init[i - 1];
     }
-    free(seq_u_prev);
-    free(seq_u_curr);
-    free(seq_u_next);
+    for(i = 1; i < nsteps; i++){
+      oracle[i][0] = 0;
+      oracle[i][nx + 1] = 0; 
+      // update
+      for (j = 1; j < nx + 1; j++)
+	oracle[i+1][j] = 2.0*oracle[i][j] - oracle[i-1][j] +
+	  k*(oracle[i][j+1] + oracle[i][j-1] -2.0*oracle[i][j]);
+    }
   }
 
 #endif
@@ -179,15 +163,22 @@ void initialization() {
   assert(u_curr);
   u_next = (double *)malloc((nxl + 2) * sizeof(double));
   assert(u_next);
+  // sets constant boundaries
+  u_prev[0] = 0;
+  u_curr[0] = 0;
+  u_next[0] = 0;
+  u_prev[nxl + 1] = 0;
+  u_curr[nxl + 1] = 0;
+  u_next[nxl + 1] = 0;
   // skip processes which are allocated no cells
-  if(first != 0)
+  if(first != 0 && nxl != 0)
     left = OWNER(first - 1);
   else 
-    left = OWNER(nx-1);
-  if(first + nxl < nx)
+    left = MPI_PROC_NULL;
+  if(first + nxl < nx && nxl != 0)
     right = OWNER(first + nxl);
   else
-    right = OWNER(0);
+    right = MPI_PROC_NULL;
 }
 
 /* Print out the value of data cells; 
@@ -199,12 +190,12 @@ void printData (int time, int first, int length, double * buf) {
     printf("u_curr[%d]=%8.8f   ", first + i, buf[i]);
 #ifdef _CIVL
 
-    $assert (oracle[time][first + i] == buf[i]):			\
+     $assert (oracle[time + 1][first + i + 1] == buf[i]):		\
     "Error: disagreement at time %d position %d: saw %lf, expected %lf", \
-    time, first + i, buf[i], oracle[time][first + i];
+    time, first + i, buf[i], oracle[time + 1][first + i + 1];
 
 #endif
-    if(i%2 == 0) printf("\n");
+    printf("\n");
   }
 }
 
@@ -214,7 +205,7 @@ void write_frame (int time) {
     double  buf[nx + 2];
     MPI_Status status;
 
-    printf("======= Time %d =======\n", time);
+    printf("\n======= Time %d =======\n", time);
     printData(time, first, nxl, &u_curr[1]);
     for(int i=1; i < nprocs; i++) {
       int displ = firstForProc(i);
@@ -240,30 +231,36 @@ void exchange(){
 int main(int argc, char * argv[]) {
   int iter;
 
+
+#ifdef _CIVL
   // elaborate nx to concrete value...
-  for(int i=0; i<nx; i++);
+  elaborate(nx);
+
+#endif
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs); 
   initialization();
-  if(rank == 0) {
-    // Send every process their cells
-    for(int i=1; i < nprocs; i++) {
-      int first = firstForProc(i);
-      int count = countForProc(i);
+#ifdef _CIVL
 
-      MPI_Send(&u_init[first], count, MPI_DOUBLE, i, DATAPASS, MPI_COMM_WORLD);
+  if(nxl != 0) {
+    for(int i=1; i<nxl+1; i++) {
+       u_prev[i] = u_init[first + i - 1];
+       u_curr[i] = u_init[first + i - 1];
     }
-    memcpy(&u_prev[1], u_init, sizeof(double) * nxl);
-    memcpy(&u_curr[1], u_init, sizeof(double) * nxl);
-  } else {
-    MPI_Recv(&u_curr[1], nxl, MPI_DOUBLE, 0, DATAPASS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    memcpy(&u_prev[1], &u_curr[1], sizeof(double) * nxl);
   }
+#else
+
+  if(nxl != 0) {
+    init(first, nxl);
+    memcpy(&u_curr[1], &u_prev[1], nxl * sizeof(double));
+  }
+
+#endif
   for(iter = 0; iter < nsteps; iter++) {
-    if(iter % wstep == 0)
-      write_frame(iter);
     if(nxl != 0){
+      if(iter % wstep == 0)
+	write_frame(iter);
       exchange();
       update();
     }
