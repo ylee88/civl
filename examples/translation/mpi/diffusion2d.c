6280cc685ca8a341f3db361b6bb0a32400ba41ec
Ziqing Luo <ziqing@udel.edu>
2014-12-18 21:43:01 +0000

    changed diffusion2d.c and Makefile
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1747 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/translation/mpi/diffusion2d.c
+++ examples/translation/mpi/diffusion2d.c
@@ -1,7 +1,7 @@
-/* diffusion2d_cb.c: parallel 2d-diffusion equation solver with constant boundaries 
+/* diffusion2d.c: parallel 2d-diffusion equation solver with constant boundaries 
  * slicing matrix as a checker board.
- * To execute: mpicc diffusion2d_cb.c ; mpiexec -n 4 ./a.out 2 2
- * To verify: civl verify diffusion2d_cb.c
+ * To execute: mpicc diffusion2d.c ; mpiexec -n 4 ./a.out 2 2
+ * To verify: civl verify diffusion2d.c
  */
 #include<stdio.h>
 #include<stdlib.h>
@@ -19,58 +19,68 @@
 
 #ifdef _CIVL
 
-$input int NXB = 5;                // nx upper bound
-$input int nx;                     // global number of columns in matrix
+$input long NXB = 5;               //nx upper bound
+$input long nx;                    //global number of columns in matrix
 $assume 1 <= nx && nx <= NXB;
-$input int NYB = 5;                // ny upper bound
-$input int ny;                     // global number of rows of matrix
+$input long NYB = 5;               //ny upper bound
+$input long ny;                    //global number of rows of matrix
 $assume 1 <= ny && ny <= NYB;
-$input double u_init[ny+2][nx+2];  // initial value of temperatures, includes 4 
-                                   // strings of constant boundries
-$input double k;                   // constant coefficient  
+$input double u_init[ny+2][nx+2];  //initial value of temperatures, including boundaries
+$input double k;                   //constant coefficient  
 $assume k > 0.0 && k < 0.5;
-$input int NSTEPSB = 5;            // boundary of number of steps
-$input int nsteps;                 // number of steps
+$input int NSTEPSB = 3;            //boundary of number of steps
+$input int nsteps;                 //number of steps
 $assume 1<=nsteps && nsteps<=NSTEPSB;
-$input int wstep = 1;              // write frame every this many time steps
-double oracle[nsteps][ny+2][nx+2]; // solution computed sequentially, done by proc 0 only
-$input int XPROCSB;                // Bound number of components of columns
-$input int NPROCSX;                 // Number of components of columns
-$assume NPROCSX > 1 && NPROCSX <= XPROCSB;
-$input int YPROCSB;                // Bound number of components of rows
-$input int NPROCSY;                 // Number of components of rows
-$assume NPROCSY > 1 && NPROCSY <= YPROCSB;
-$input int _NPROCS;
+$input int wstep = 1;              //write frame every this many time steps
+double oracle[nsteps][ny+2][nx+2]; //solution computed sequentially, done by proc 0 only
+$input int NPROCSXB;               //bound number of components of columns
+$input int NPROCSX = 2;            //number of components of columns
+$assume NPROCSX > 1 && NPROCSX <= NPROCSXB;
+$input int NPROCSYB;               //bound number of components of rows
+$input int NPROCSY = 2;            //number of components of rows
+$assume NPROCSY > 1 && NPROCSY <= NPROCSYB;
+$input int _NPROCS = NPROCSX * NPROCSY;
+
 #else
 
-int nx, ny, nsteps, wstep;
+long nx, ny;
+int nsteps, wstep;
 int NPROCSX, NPROCSY;
-double constTemp, initTemp;       // values of constant boundaries and 
-                                  // initial tempretures
+double constTemp;                  //value of constant boundaries for test
+double initTemp;                   //value of initial tempretrue for test
 double k;
 
 #endif
 
 /* Global variables */
+int nprocs, rank, left, right, top, bottom;
+int nxl, nyl;
+long firstCol, firstRow;
 double ** u_curr;
 double ** u_next;
-int nprocs, rank, left, right, top, bottom;
-int nxl, nyl, firstCol, firstRow;
 
+/* Following book keeping functions are designed based on such orgnization:
+ * row 0: 0 1 2
+ * row 1: 3 4 5 
+ * ...          */
 /* Compute the global column index of cells owned by the process */
-int firstColForProc(int rank) {
-  return (rank - (rank / NPROCSX)*NPROCSX)*nx/NPROCSX;
+long firstColForProc(int rank) {
+  long tmp = (rank - (rank / NPROCSX)*NPROCSX)*nx;
+
+  return tmp/NPROCSX;
 }
 
 /* Compute the global row index of cells owned by the process */
-int firstRowForProc(int rank) {
-  return ((rank / NPROCSX)*ny)/NPROCSY;
+long firstRowForProc(int rank) {
+  long tmp = ((rank / NPROCSX)*ny);
+
+  return tmp/NPROCSY;
 }
 
 /* Computes the number of columns owned by the process */
 int countColForProc(int rank) {
-  int a = firstColForProc(rank);
-  int b;
+  long a = firstColForProc(rank);
+  long b;
 
   if((rank / NPROCSX) == ((rank+1) / NPROCSX))
     b = firstColForProc(rank+1);
@@ -81,54 +91,60 @@ int countColForProc(int rank) {
 
 /* Computes the number of rows owned by the process */
 int countRowForProc(int rank) {
-  int a = firstRowForProc(rank);
-  int b = firstRowForProc(rank+NPROCSX);
+  long a = firstRowForProc(rank);
+  long b = firstRowForProc(rank+NPROCSX);
 
   return b - a;
 }
 
 /* Get the owner process of the given cell */
-int OWNER(int col, int row) {
-  int procRow = ((NPROCSY * (row+1))-1) / ny;
-  int procCol = ((col + 1) * NPROCSX - 1) / nx;
+int OWNER(long col, long row) {
+  long tmp = ((NPROCSY * (row+1))-1);
+  int procRow = tmp / ny;
+  int procCol;
 
-  return procRow * NPROCSX + procCol;
+  tmp = ((col + 1) * NPROCSX - 1);
+  procCol  = tmp / nx;
+  tmp = procRow * NPROCSX;
+  return tmp + procCol;
 }
 
 
+/* initialize all data values owned by each process */
+void initData() {
 #ifdef _CIVL
-void setConstBoundaries() {
-  int i;
 
   // sets vertical constant boundaries
   if(left == MPI_PROC_NULL)
-    for(i=0; i<nyl+2; i++) {
-      u_curr[i][0] = u_init[i + firstRow][0];
-      u_next[i][0] = u_curr[i][0];
-    }
+    for(int i=0; i<nyl+2; i++)
+      u_next[i][0] = u_curr[i][0] = u_init[i + firstRow][0];
   if(right == MPI_PROC_NULL)
-    for(i=0; i<nyl+2; i++) {
-      u_curr[i][nxl+1] = u_init[i + firstRow][nx+1];
-      u_next[i][nxl+1] = u_curr[i][nxl+1];
-    }
+    for(int i=0; i<nyl+2; i++)
+      u_next[i][nxl+1] = u_curr[i][nxl+1] = u_init[i + firstRow][nx+1];
   // sets horizontal constant boundaries
   if(top == MPI_PROC_NULL)
-    for(i=0; i<nxl+2; i++) {
-      u_curr[0][i] = u_init[0][i + firstCol];
-      u_next[0][i] = u_curr[0][i];
-    }
+    for(int i=0; i<nxl+2; i++)
+      u_next[0][i] = u_curr[0][i] = u_init[0][i + firstCol];
   if(bottom == MPI_PROC_NULL)
-    for(i=0; i<nxl+2; i++) {
-      u_curr[nyl+1][i] = u_init[ny+1][i + firstCol];
-      u_next[nyl+1][i] = u_curr[nyl+1][i];
-    }
-}
+    for(int i=0; i<nxl+2; i++)
+      u_next[nyl+1][i] = u_curr[nyl+1][i] = u_init[ny+1][i + firstCol];
+  for(int i=1; i<nyl+1; i++)
+    memcpy(&u_curr[i][1], &u_init[firstRow + i][firstCol + 1], nxl * sizeof(double));
+
+#else
+
+  for(int i=0; i < nyl+2; i++)
+    for(int j=0; j < nxl+2; j++)
+      if(i == 0 || j == 0 || i == nyl+1 || j == nxl+1)
+	u_next[i][j] = u_curr[i][j] = constTemp;
+      else
+	u_curr[i][j] = initTemp;
+
 #endif
+}
 
 /* Initialize all global variables */
 void initialization(int argc, char * argv[]) {
-  int i,j;
-
 #ifndef _CIVL
 
   nsteps = 300;
@@ -136,19 +152,22 @@ void initialization(int argc, char * argv[]) {
   nx = 15;
   ny = 15;
   if(argc < 3) {
-    printf("Program needs 2 arguments to specify the number of sliced components in x axis and y axis:\n"
-	   "It should go with the format: mpiexec -n [nprocs] "
-	   "[filename] [#components in x axis] [#components in y axis]\n");
+    printf("Program needs 2 arguments to specify the number of processes in x direction and y direction:\n"
+	   "It should go with the format: mpiexec -n NPROCS diffusion2d "
+	   "NPROCSX NPROCSY\n"
+	   "NPROCSX: number of processes in x direction\n"
+	   "NPROCSY: number of processes in y direction\n"
+	   "NPROCS: the product of NPROCSX and NPROCSY\n");
     assert(0);
   }
   NPROCSX = atoi(argv[1]); 
   NPROCSY = atoi(argv[2]);
-  assert(0 < NPROCSX * NPROCSY <=nprocs);
-  nprocs = (nprocs > NPROCSX * NPROCSY)?NPROCSX*NPROCSY:nprocs;
+  assert(0 < NPROCSX * NPROCSY && 
+	 NPROCSX * NPROCSY <=nprocs);
   constTemp = 0.0;
   initTemp = 100.0;
   k = 0.13;
-  printf("Diffusion2d with k=%f, nx=%d, ny=%d, nsteps=%d, wstep=%d\n",
+  printf("Diffusion2d with k=%f, nx=%ld, ny=%ld, nsteps=%d, wstep=%d\n",
 	 k, nx, ny, nsteps, wstep);
 
 #endif
@@ -159,7 +178,7 @@ void initialization(int argc, char * argv[]) {
   assert(u_curr);
   u_next = (double **)malloc((nyl + 2) * sizeof(double *));
   assert(u_next);
-  for(i=0; i < nyl + 2; i++){
+  for(int i=0; i < nyl + 2; i++){
     u_curr[i] = (double *)malloc((nxl + 2) * sizeof(double));
     assert(u_curr[i]);
     u_next[i] = (double *)malloc((nxl + 2) * sizeof(double));
@@ -184,17 +203,18 @@ void initialization(int argc, char * argv[]) {
     bottom = OWNER(firstCol, firstRow + nyl);
   else
     bottom = MPI_PROC_NULL;
+
 #ifdef _CIVL
-  setConstBoundaries();
+
   // In CIVL mode process with rank 0 will be responsible for computing the diffusion2d equation 
   // sequentially such that the results can be used to compare with the ones of parallel run.
   if(rank == 0) {
-    for(i = 0; i < ny + 2; i++)
-      for(j = 0; j < nx + 2; j++)
+    for(long i = 0; i < ny + 2; i++)
+      for(long j = 0; j < nx + 2; j++)
 	oracle[0][i][j] = u_init[i][j];
     for(int t=1; t < nsteps; t++)
-      for(i = 0; i < ny + 2; i++)
-	for(j = 0; j < nx + 2; j++)
+      for(long i = 0; i < ny + 2; i++)
+	for(long j = 0; j < nx + 2; j++)
 	  if(i==0 || j==0 || i == ny + 1 || j == nx + 1)
 	    oracle[t][i][j] = oracle[t-1][i][j];
 	  else
@@ -207,11 +227,10 @@ void initialization(int argc, char * argv[]) {
 }
 
 void update() {
-  int i, j;
   double **tmp;
 
-  for(i = 1; i < nyl + 1; i++)
-    for(j = 1; j < nxl + 1; j++) {
+  for(int i = 1; i < nyl + 1; i++)
+    for(int j = 1; j < nxl + 1; j++) {
       u_next[i][j] = u_curr[i][j] +
 	k*(u_curr[i+1][j] + u_curr[i-1][j] + 
 	   u_curr[i][j+1] + u_curr[i][j-1] - 4*u_curr[i][j]);
@@ -246,57 +265,55 @@ void exchange() {
     for(int i = 0; i < nyl; i++) u_curr[i+1][0] = recvbuf[i];
 }
 
-void printData(int time, int firstCol, int nxl, int firstRow, int nyl, double * buf) {
+void printData(int time, int firstCol, int nxl, int currRow, double * buf) {
 
-  for(int i=0; i<nyl; i++) {
-    for(int j=0; j<nxl; j++) {
-      printf("%8.2f ", *(buf + i*nxl + j));
+  for(int i=0; i<nxl; i++) {
+    printf("%6.2f", *(buf + i));
 #ifdef _CIVL
-    $assert(*(buf + i*nxl + j) == oracle[time][firstRow + i + 1][firstCol + j + 1]) : \
+  $assert(*(buf + i) == oracle[time][currRow + 1][firstCol + i + 1]) : \
     "Error: disagreement at time %d position [%d][%d]: saw %lf, expected %lf", \
-      time, firstRow + i, firstCol + j, 
-      *(buf + i*nxl + j), oracle[time][firstRow + i + 1][firstCol + j + 1];
+      time, currRow, firstCol + i, 
+      *(buf + i), oracle[time][currRow + 1][firstCol + i + 1];
 #endif
-    }
-    printf("\n");
   }
 }
 
 void write_frame(int time) {
   double * buf; // buffer of data to print
-  int i, j;
 
-  buf = (double *)malloc(nxl * nyl * sizeof(double));
-  // writes data into buffer array
-  for(i = 0; i < nyl; i++)
-    for(j = 0; j < nxl; j++) {
-      buf[i*nxl + j] = u_curr[i+1][j+1];
+  buf = (double *)malloc(nxl * sizeof(double));
+  //sends data row by row
+  if(rank != 0){
+    for(int i=0; i<nyl; i++) {
+      memcpy(buf, &u_curr[i+1][1], nxl*sizeof(double));
+      MPI_Send(buf, nxl, MPI_DOUBLE, 0, DATAPASS, comm);
     }
+  }
+  free(buf);
   if(rank == 0) {
     printf("\n-------------------- time step:%d --------------------\n", time);
-    printData(time, firstCol, nxl, firstRow, nyl, buf);
-    free(buf);
-    for(i=1; i<nprocs; i++){
-      double * recvbuf;
-      int senderx, sendery;
-      int senderNyl, senderNxl;
+    for(int i=0; i < NPROCSY; i++) {
+      int numRows = countRowForProc(i*NPROCSX);
 
-      senderNxl = countColForProc(i);
-      senderNyl = countRowForProc(i);
-      if(senderNxl != 0 && senderNyl != 0) {
-	recvbuf = (double *)malloc(senderNxl * senderNyl * sizeof(double));
-	senderx = firstColForProc(i);
-	sendery = firstRowForProc(i);
-	MPI_Recv(recvbuf, senderNyl*senderNxl, MPI_DOUBLE, i, 
-		 DATAPASS, comm, MPI_STATUS_IGNORE);
-	printData(time, senderx, senderNxl, sendery, senderNyl, recvbuf);
-	free(recvbuf);
+      for(int j=0; j < numRows; j++) {
+	for(int k=i*NPROCSX; k < (i+1)*NPROCSX; k++) {
+	  if(k!=0){
+	    int senderx = firstColForProc(k);
+	    int sendery = firstRowForProc(k);
+	    int senderNxl = countColForProc(k);
+	    double * recvbuf = (double *)malloc(senderNxl * sizeof(double));
+
+	    MPI_Recv(recvbuf, senderNxl, MPI_DOUBLE, k, DATAPASS, comm, MPI_STATUS_IGNORE);
+	    printData(time, senderx, senderNxl, sendery+j, recvbuf);
+	    free(recvbuf);
+	  } else {
+	    printData(time, firstCol, nxl, firstRow+j, &u_curr[j+1][1]);
+	  }
+	}
+	printf("\n");
       }
     }
-  } else {
-    MPI_Send(buf, nyl*nxl, MPI_DOUBLE, 0, DATAPASS, comm);
-    free(buf);
-  }
+  } 
 }
 
 int main(int argc, char * argv[]) {
@@ -315,21 +332,7 @@ int main(int argc, char * argv[]) {
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(comm, &nprocs);
   initialization(argc, argv);
-#ifdef _CIVL
-
-  for(i=1; i<nyl+1; i++)
-    memcpy(&u_curr[i][1], &u_init[firstRow + i][firstCol + 1], nxl * sizeof(double));
-
-#else
-
-  for(int i=0; i < nyl+2; i++)
-    for(int j=0; j < nxl+2; j++)
-      if(i == 0 || j == 0 || i == nyl+1 || j == nxl+1)
-	u_next[i][j] = u_curr[i][j] = constTemp;
-      else
-	u_curr[i][j] = initTemp;
-
-#endif
+  initData();
   for(i=0; i<nsteps; i++) {
     if(nxl != 0 && nyl != 0) {
       if(i%wstep == 0)
