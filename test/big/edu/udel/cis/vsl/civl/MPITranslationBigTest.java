9741ccbd30142bffb84394abf3f2eb0ae590d173
Ziqing Luo <ziqing@udel.edu>
2014-12-23 16:22:44 +0000

    clean up imports
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1759 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/big/edu/udel/cis/vsl/civl/MPITranslationBigTest.java
+++ test/big/edu/udel/cis/vsl/civl/MPITranslationBigTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.udel.cis.vsl.civl.run.IF.UserInterface;
@@ -34,22 +33,27 @@ public class MPITranslationBigTest {
 
 	@Test
 	public void mpi_wave1d() {
-		assertTrue(ui.run("verify", "-enablePrintf=false", filename("wave1d.c")));
+		assertTrue(ui
+				.run("verify", "-enablePrintf=false", filename("wave1d.c")));
 	}
-	
+
 	@Test
 	public void mpi_diff1d() {
-		assertTrue(ui.run("verify", "-enablePrintf=false", filename("diffusion1d.c")));
+		assertTrue(ui.run("verify", "-enablePrintf=false",
+				filename("diffusion1d.c")));
 	}
-	
+
 	@Test
 	public void mpi_diff2d() {
-		assertTrue(ui.run("verify", "-enablePrintf=false", "-input_NPROCS=4", "-inputNPROCSX=2", "-inputNPROCSY=2", "-inputNSTEPSB=3", filename("diffusion2d.c")));
+		assertTrue(ui.run("verify", "-enablePrintf=false", "-input_NPROCS=4",
+				"-inputNPROCSX=2", "-inputNPROCSY=2", "-inputNSTEPSB=3",
+				filename("diffusion2d.c")));
 	}
-	
+
 	@Test
 	public void mpi_pi() {
-		assertTrue(ui.run("verify", "-enablePrintf=false", filename("mpi_pi.c")));
+		assertTrue(ui
+				.run("verify", "-enablePrintf=false", filename("mpi_pi.c")));
 	}
 
 }
