7c66e72ec5d706d5caeb841d0a6c3b7c48021fdf
Stephen Siegel <siegel@udel.edu>
2015-01-05 13:57:10 +0000

    @Ignoreing tests in OpenMPCIVLTransformerTest because they only print voluminous output.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1801 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/regress/edu/udel/cis/vsl/civl/OpenMP2CIVLTransformerTest.java
+++ test/regress/edu/udel/cis/vsl/civl/OpenMP2CIVLTransformerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.udel.cis.vsl.civl.run.IF.UserInterface;
@@ -24,30 +25,35 @@ public class OpenMP2CIVLTransformerTest {
 
 	/* **************************** Test Methods *************************** */
 
+	@Ignore
 	@Test
 	public void dotProduct1() {
 		assertTrue(ui.run("show -showModel", "-ompNoSimplify",
 				"-inputTHREAD_MAX=4", filename("dotProduct1.c")));
 	}
 
+	@Ignore
 	@Test
 	public void dotProductCritical() {
 		assertTrue(ui.run("show -showModel", "-ompNoSimplify",
 				"-inputTHREAD_MAX=4", filename("dotProduct_critical.c")));
 	}
 
+	@Ignore
 	@Test
 	public void matProduct1() {
 		assertTrue(ui.run("show -showModel", "-ompNoSimplify",
 				"-inputTHREAD_MAX=4", filename("matProduct1.c")));
 	}
 
+	@Ignore
 	@Test
 	public void parallelfor() {
 		assertTrue(ui.run("show -showModel", "-ompNoSimplify",
 				"-inputTHREAD_MAX=4", filename("parallelfor.c")));
 	}
 
+	@Ignore
 	@Test
 	public void raceCond1() {
 		assertTrue(ui.run("show -showModel", "-ompNoSimplify",
