bc6d607999b83054b56eef4f25b4cc1893901f16
Manchun Zheng <zmanchun@gmail.com>
2015-01-08 16:56:16 +0000

    fixed the example path according to the recent re-organization of examples.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1819 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/dev/edu/udel/cis/vsl/civl/HybridTest.java
+++ test/dev/edu/udel/cis/vsl/civl/HybridTest.java
@@ -13,7 +13,7 @@ import edu.udel.cis.vsl.civl.run.IF.UserInterface;
 public class HybridTest {
 	/* *************************** Static Fields *************************** */
 
-	private static File rootDir = new File(new File("examples"), "translation");
+	private static File rootDir = new File(new File("examples"), "mpi-pthread");
 
 	private static UserInterface ui = new UserInterface();
 
@@ -31,7 +31,7 @@ public class HybridTest {
 		// filename("mpi-pthread/mpithreads_both.c"),
 		// "-input__NPROCS=3", "-showInputs", "-enablePrintf=false"));
 		ui.run("show", "-showProgram",
-				filename("mpi-pthread/mpithreads_both.c"));
+				filename("mpithreads_both.c"));
 		// ui.run("verify", filename("mpi-pthread/mpithreads_both.c"),
 		// "-input__NPROCS=3", "-showInputs", "-enablePrintf=false");
 		// ui.run("replay", filename("mpi-pthread/mpithreads_both.c"), "-gui");
@@ -40,7 +40,7 @@ public class HybridTest {
 	@Test
 	public void mpi_pthreads_pie_collective() throws ABCException {
 		assertTrue(ui.run("verify", "-input__NPROCS=2", "-enablePrintf=false",
-				filename("mpi-pthread/mpi-pthreads-pie-collective.c")));
+				filename("mpi-pthreads-pie-collective.c")));
 	}
 	
 	@AfterClass
