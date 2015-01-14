18ef3c34c1a597f471ce22c5b6acf988c26581a8
Manchun Zheng <zmanchun@gmail.com>
2015-01-14 19:01:59 +0000

    added a simple hybrid test.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@1826 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/dev/edu/udel/cis/vsl/civl/HybridTest.java
+++ test/dev/edu/udel/cis/vsl/civl/HybridTest.java
@@ -30,19 +30,25 @@ public class HybridTest {
 		// assertTrue(ui.run("verify",
 		// filename("mpi-pthread/mpithreads_both.c"),
 		// "-input__NPROCS=3", "-showInputs", "-enablePrintf=false"));
-		ui.run("show", "-showProgram",
-				filename("mpithreads_both.c"));
-		// ui.run("verify", filename("mpi-pthread/mpithreads_both.c"),
-		// "-input__NPROCS=3", "-showInputs", "-enablePrintf=false");
-		// ui.run("replay", filename("mpi-pthread/mpithreads_both.c"), "-gui");
+		ui.run("show", "-showProgram", filename("mpithreads_both.c"));
+		ui.run("verify", "-input_NPROCS=3", "-showInputs",
+				"-enablePrintf=true", filename("mpithreads_both.c"));
 	}
 
 	@Test
 	public void mpi_pthreads_pie_collective() throws ABCException {
-		assertTrue(ui.run("verify", "-input__NPROCS=2", "-enablePrintf=false",
+		assertTrue(ui.run("verify", "-input_NPROCS=2", "-enablePrintf=false",
 				filename("mpi-pthreads-pie-collective.c")));
 	}
-	
+
+	@Test
+	public void helloWorld() throws ABCException {
+		// ui.run("run", "-input_NPROCS=3", "-showTransitions=false",
+		// filename("helloWorld.c"));
+		// ui.run("show", "-showProgram", filename("helloWorld.c"));
+		ui.run("verify", "-input_NPROCS=2", filename("helloWorld.c"));
+	}
+
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		ui = null;
