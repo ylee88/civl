cae41ef1346760f35c55a06da0cb1c0cdee6c559
Ziqing Luo <ziqing@udel.edu>
2015-03-24 15:37:45 +0000

    divide MPICollective test into 2 classes
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2156 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/regress/edu/udel/cis/vsl/civl/MPICollectiveTest_part2.java
+++ test/regress/edu/udel/cis/vsl/civl/MPICollectiveTest_part2.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 import edu.udel.cis.vsl.civl.run.IF.UserInterface;
 
-public class MPICollectiveTest {
+public class MPICollectiveTest_part2 {
 
 	/* *************************** Static Fields *************************** */
 
@@ -26,119 +26,6 @@ public class MPICollectiveTest {
 	}
 
 	/* **************************** Test Methods *************************** */
-
-	@Test
-	public void vectorSum() {
-		assertTrue(ui.run("verify -input_NPROCS=5", filename("vectorSum.c")));
-	}
-
-	@Test
-	public void vectorSum_bad() {
-		assertFalse(ui.run("verify -input_NPROCS=5",
-				filename("vectorSum_bad.c")));
-	}
-
-	@Test
-	public void bcast_bad() {
-		assertFalse(ui.run("verify -input_NPROCS=6 ", filename("bcast_bad.c")));
-	}
-
-	@Test
-	public void bcast_bad_but_ok() {
-		assertTrue(ui.run("verify -input_NPROCS=4 ", filename("bcast_bad.c")));
-	}
-
-	@Test
-	public void bcast_good() {
-		assertFalse(ui.run("verify -DFASSERT -input_NPROCS=3 ",
-				filename("bcast_good.c")));
-		assertTrue(ui.run("verify -input_NPROCS=3", filename("bcast_good.c")));
-	}
-
-	@Test
-	public void BcastReduce() {
-		assertTrue(ui.run("verify -input_NPROCS=5 ", filename("BcastReduce.c")));
-	}
-
-	@Test
-	public void BcastReduce_bad() {
-		assertFalse(ui.run("verify -input_NPROCS=10 ",
-				filename("BcastReduce_bad.c")));
-	}
-
-	@Test
-	public void BcastReduce2() {
-		assertTrue(ui
-				.run("verify -input_NPROCS=5 ", filename("BcastReduce2.c")));
-	}
-
-	@Test
-	public void BcastReduce2_bad() {
-		assertFalse(ui.run("verify -input_NPROCS=10 ",
-				filename("BcastReduce2.c")));
-	}
-
-	@Test
-	public void gather_order() {
-		assertFalse(ui.run("verify -input_NPROCS=6 ", filename("gather.c")));
-	}
-
-	@Test
-	public void gather_type() {
-		assertFalse(ui.run("verify -DTYPE -input_NPROCS=6 ",
-				filename("gather.c")));
-	}
-
-	@Test
-	public void gather_root() {
-		assertFalse(ui.run("verify -DROOT -input_NPROCS=6 ",
-				filename("gather.c")));
-	}
-
-	@Test
-	public void gather_good() {
-		assertTrue(ui.run("verify -input_NPROCS=2 ", filename("gather.c")));
-	}
-
-	@Test
-	public void scatter_good() {
-		assertTrue(ui.run("verify -input_NPROCS=2 ", filename("scatter.c")));
-	}
-
-	@Test
-	public void scatter_order() {
-		assertFalse(ui.run("verify -input_NPROCS=6 ", filename("scatter.c")));
-	}
-
-	@Test
-	public void scatter_type() {
-		assertFalse(ui.run("verify -DTYPE -input_NPROCS=6 ",
-				filename("scatter.c")));
-	}
-
-	@Test
-	public void scatter_root() {
-		assertFalse(ui.run("verify -DROOT -input_NPROCS=6 ",
-				filename("scatter.c")));
-	}
-
-	@Test
-	public void scatter2() {
-		assertFalse(ui.run("verify -input_NPROCS=6 ", filename("scatter2.c")));
-	}
-
-	@Test
-	public void scatterAllgather_bad() {
-		assertFalse(ui.run("verify -input_NPROCS=6 ",
-				filename("scatterAllgather_bad.c")));
-	}
-
-	@Test
-	public void scatterAllgather() {
-		assertTrue(ui.run("verify -input_NPROCS=6 ",
-				filename("scatterAllgather.c")));
-	}
-
 	@Test
 	public void scatterGather_bad() {
 		assertFalse(ui.run("verify -DORDER -input_NPROCS=6 ",
