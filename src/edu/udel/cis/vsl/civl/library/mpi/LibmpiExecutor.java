c69f7d3cb64bfafdeb20ecf72d91eeb489ca4748
Ziqing Luo <ziqing@udel.edu>
2015-03-31 18:56:57 +0000

    add data type checking for MPI
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2179 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/library/mpi/LibmpiExecutor.java
+++ src/edu/udel/cis/vsl/civl/library/mpi/LibmpiExecutor.java
@@ -9,7 +9,11 @@ import edu.udel.cis.vsl.abc.util.IF.Pair;
 import edu.udel.cis.vsl.civl.config.IF.CIVLConfiguration;
 import edu.udel.cis.vsl.civl.dynamic.IF.SymbolicUtility;
 import edu.udel.cis.vsl.civl.library.common.BaseLibraryExecutor;
+import edu.udel.cis.vsl.civl.log.IF.CIVLExecutionException;
+import edu.udel.cis.vsl.civl.model.IF.CIVLException.Certainty;
+import edu.udel.cis.vsl.civl.model.IF.CIVLException.ErrorKind;
 import edu.udel.cis.vsl.civl.model.IF.CIVLInternalException;
+import edu.udel.cis.vsl.civl.model.IF.CIVLSource;
 import edu.udel.cis.vsl.civl.model.IF.Identifier;
 import edu.udel.cis.vsl.civl.model.IF.ModelFactory;
 import edu.udel.cis.vsl.civl.model.IF.Scope;
@@ -17,6 +21,7 @@ import edu.udel.cis.vsl.civl.model.IF.expression.Expression;
 import edu.udel.cis.vsl.civl.model.IF.expression.LHSExpression;
 import edu.udel.cis.vsl.civl.model.IF.statement.CallOrSpawnStatement;
 import edu.udel.cis.vsl.civl.model.IF.statement.Statement;
+import edu.udel.cis.vsl.civl.model.IF.type.CIVLType;
 import edu.udel.cis.vsl.civl.model.IF.variable.Variable;
 import edu.udel.cis.vsl.civl.semantics.IF.Evaluation;
 import edu.udel.cis.vsl.civl.semantics.IF.Executor;
@@ -28,7 +33,13 @@ import edu.udel.cis.vsl.civl.state.IF.DynamicScope;
 import edu.udel.cis.vsl.civl.state.IF.StackEntry;
 import edu.udel.cis.vsl.civl.state.IF.State;
 import edu.udel.cis.vsl.civl.state.IF.UnsatisfiablePathConditionException;
+import edu.udel.cis.vsl.sarl.IF.Reasoner;
+import edu.udel.cis.vsl.sarl.IF.ValidityResult.ResultType;
+import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericExpression;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression.SymbolicOperator;
+import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
 
 /**
  * Implementation of system functions declared mpi.h.
@@ -76,6 +87,7 @@ public class LibmpiExecutor extends BaseLibraryExecutor implements
 		SymbolicExpression[] argumentValues;
 		CallOrSpawnStatement call;
 		int numArgs;
+		String process = state.getProcessState(pid).name() + "(id=" + pid + ")";
 
 		if (!(statement instanceof CallOrSpawnStatement)) {
 			throw new CIVLInternalException("Unsupported statement for mpi",
@@ -103,6 +115,10 @@ public class LibmpiExecutor extends BaseLibraryExecutor implements
 			break;
 		case "CMPI_Get_status":
 			state = executeGetStatus(state, pid, call);
+			break;
+		case "CMPI_AssertConsistentType":
+			state = executeAssertConsistentType(state, pid, process, arguments,
+					argumentValues);
 			break;
 		default:
 			throw new CIVLInternalException("Unknown civlc function: " + name,
@@ -252,5 +268,74 @@ public class LibmpiExecutor extends BaseLibraryExecutor implements
 				return currDySid;
 		}
 		return -1;
+	}
+
+	/**
+	 * Executing the function
+	 * <code>CMPI_AssertConsistentType(void * ptr, int sizeofDatatype)</code>
+	 * The function checks if the pointer points to a object whose size of data
+	 * type is consistent with the given size of data type.
+	 * 
+	 * @param state
+	 *            The current state
+	 * @param pid
+	 *            The PID of the process
+	 * @param process
+	 *            The String identifier of the process
+	 * @param arguments
+	 *            {@link Expression}s of arguments of the system function
+	 * @param argumentValues
+	 *            {@link SymbolicExpression}s of arguments of the system
+	 *            function
+	 * @return
+	 * @throws UnsatisfiablePathConditionException
+	 */
+	private State executeAssertConsistentType(State state, int pid,
+			String process, Expression[] arguments,
+			SymbolicExpression[] argumentValues)
+			throws UnsatisfiablePathConditionException {
+		CIVLSource ptrSource = arguments[0].getSource();
+		SymbolicExpression pointer = argumentValues[0];
+		NumericExpression assertedTypesize = (NumericExpression) argumentValues[1];
+		NumericExpression realTypesize;
+		CIVLType realType;
+		SymbolicType realSymType;
+		BooleanExpression claim;
+		Reasoner reasoner;
+		ResultType resultType;
+
+		if (!pointer.operator().equals(SymbolicOperator.CONCRETE)
+				|| !symbolicUtil.isValidPointer(pointer)) {
+			errorLogger.reportError(new CIVLExecutionException(
+					ErrorKind.POINTER, Certainty.CONCRETE, process,
+					"Attempt to read/write a invalid pointer type variable",
+					arguments[0].getSource()));
+			return state;
+		}
+		if (symbolicUtil.isNullPointer(pointer))
+			return state;
+		realType = symbolicAnalyzer.getFlattenedArrayElementType(state,
+				ptrSource, pointer);
+		realSymType = realType.getDynamicType(universe);
+		realTypesize = symbolicUtil.sizeof(ptrSource, realSymType);
+		claim = universe.equals(realTypesize, assertedTypesize);
+		reasoner = universe.reasoner(state.getPathCondition());
+		resultType = reasoner.valid(claim).getResultType();
+		if (!resultType.equals(ResultType.YES)) {
+			this.errorLogger
+					.logError(
+							ptrSource,
+							state,
+							process,
+							symbolicAnalyzer.stateToString(state),
+							claim,
+							resultType,
+							ErrorKind.MPI_ERROR,
+							"The primitive type:"
+									+ realType.toString()
+									+ " of the object pointed by the input pointer argument of"
+									+ " MPI routines is not consistent with the given MPI_Datatype");
+		}
+		return state;
 	}
 }
