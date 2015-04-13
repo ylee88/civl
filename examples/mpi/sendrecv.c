4e72b94610cc66a08ac799b9301f059f5ea9b7bc
Ziqing Luo <ziqing@udel.edu>
2015-04-13 14:50:38 +0000

    changed the example to let processes send recv like a ring
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2229 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/mpi/sendrecv.c
+++ examples/mpi/sendrecv.c
@@ -14,14 +14,14 @@ int main(int argc, char * argv[]) {
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  left = (rank == 0) ? MPI_PROC_NULL : rank - 1;
-  right = (rank == (size - 1)) ? MPI_PROC_NULL : rank + 1;
+  left = (rank == 0) ? size - 1 : rank - 1;
+  right = (rank == (size - 1)) ? 0 : rank + 1;
   MPI_Sendrecv(&rank, 1, MPI_INT, left, FROMRIGHT, &recv, 1, MPI_INT, 
 	       right, FROMRIGHT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //  $assert(recv == right);
+  $assert(recv == right);
   MPI_Sendrecv(&rank, 1, MPI_INT, right, FROMLEFT, &recv, 1, 
   	       MPI_INT, left, FROMLEFT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //  $assert(recv == left);
+  $assert(recv == left);
   MPI_Finalize();
   return 0;
 }
