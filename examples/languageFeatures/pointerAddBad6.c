0b1b71d2c7a23d17f40f500f036bf937f8e881f2
Ziqing Luo <ziqing@udel.edu>
2015-06-03 17:26:50 +0000

    change test example for covering more
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2323 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/languageFeatures/pointerAddBad6.c
+++ examples/languageFeatures/pointerAddBad6.c
@@ -2,7 +2,11 @@
 
 int main(int argc, char * argv[]) {
   int a[2][2][2] = {{{0,1}, {2,3}}, {{4,5}, {6,7}}};
+#ifdef ADDONOFFSET
   void *p = (&a + 1);
+#else
+  void *p = (&a + 2);
+#endif
   int *t;
 
   t = (int *)(p + 2);
