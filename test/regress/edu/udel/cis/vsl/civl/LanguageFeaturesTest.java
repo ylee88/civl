87736c6b03766e03e4850485610b44ab60d198cc
Ziqing Luo <ziqing@udel.edu>
2015-06-04 18:37:44 +0000

    changed the testing example
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2333 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/regress/edu/udel/cis/vsl/civl/LanguageFeaturesTest.java
+++ test/regress/edu/udel/cis/vsl/civl/LanguageFeaturesTest.java
@@ -311,9 +311,9 @@ public class LanguageFeaturesTest {
 
 	@Test
 	public void pointerAddBad6() throws ABCException {
-		assertFalse(ui.run("verify", filename("pointerAddBad6.c")));
-		assertFalse(ui
-				.run("verify -DADDONOFFSET", filename("pointerAddBad6.c")));
+		assertFalse(ui.run("verify -errorBound=2", filename("pointerAddBad6.c")));
+//		assertFalse(ui
+//				.run("verify -DADDONOFFSET", filename("pointerAddBad6.c")));
 	}
 
 	@Test
