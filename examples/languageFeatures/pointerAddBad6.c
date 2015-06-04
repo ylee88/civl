8777216106ed1e6f8d6a459c6bdb841ea8d02649
Ziqing Luo <ziqing@udel.edu>
2015-06-04 18:32:15 +0000

    now this example can cover more
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2332 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/languageFeatures/pointerAddBad6.c
+++ examples/languageFeatures/pointerAddBad6.c
@@ -1,15 +1,11 @@
 #include <assert.h>
-
+#include <civlc.cvh>
+$input int offset;
+$assume(0<=offset && offset<=1);
 int main(int argc, char * argv[]) {
-  int a[2][2][2] = {{{0,1}, {2,3}}, {{4,5}, {6,7}}};
-#ifdef ADDONOFFSET
-  void *p = (&a + 1);
-#else
-  void *p = (&a + 2);
-#endif
-  p += 0;
-  int *t = (int *)&a + 0 ;
+  int a = 8;
+  void *p = &a + 1;
+  void * q = p + offset;
 
-  t = (int *)(p + 2);
   return 0;
 }
