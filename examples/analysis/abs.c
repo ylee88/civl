e4cc4f5ef4155da5a0022dc99333b36c89065de1
Manchun Zheng <zmanchun@gmail.com>
2015-06-15 20:53:52 +0000

    added more tests.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2349 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/analysis/abs.c
+++ examples/analysis/abs.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <civlc.cvh>
 
-$input int i;
+$input int k;
 $input int x;
 $input int y;
 $input int z;
 $assume(x<0);
 $assume(y>0);
+$assume(k!=0);
 void main(){
     int a = 9;
     
@@ -14,8 +15,9 @@ void main(){
     a=abs(x); // -
     a=abs(y); // +
     a=abs(z); // *
+    a=abs(k);
     a=abs(x*y*z); // *
-    
+    a=abs(x*y*k);
     a=abs(x*y); // -
     $assert(x*y<=0);
     
@@ -24,4 +26,9 @@ void main(){
     
     a=abs((-x)%y); // +
     $assert((-x)%y>=0);
+    
+    a=abs(z%2-1);
+    $assert((z%2-1)<=0);
+    a=abs(z%2-2);
+    $assert((z%2-2)<0);
 }
