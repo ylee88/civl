92a5f334b01d54db9ed30a2c74ea178b1bccecb1
Manchun Zheng <zmanchun@gmail.com>
2015-06-18 18:09:38 +0000

    minor correction.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2353 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/experimental/oo.c
+++ examples/experimental/oo.c
@@ -1,4 +1,5 @@
-#include <assert.h>
+//#include <assert.h>
+#include <stdio.h>
 
 typedef struct Point {
   int (*getX)(void);
@@ -15,14 +16,23 @@ Point make_point(int x0, int y0) {
   void setX( int x1 ) { x=x1; }
   void setY( int y1 ) { y=y1; }
   
+    x=x0;
+    y=y0;
+    
   Point result = {&getX, &getY, &setX, &setY};
 
+    
   return result;
 }
 
 int main() {
   Point p1 = make_point(0,0);
   Point p2 = make_point(1,2);
-
-  assert(p1.getX()==0);
+  int k = p2.getY();
+    
+  printf("k=%d\n",k);
+    p2.setY(17000);
+    k=p2.getY();
+    printf("k=%d\n",k);
+  //assert(k==0);
 }
