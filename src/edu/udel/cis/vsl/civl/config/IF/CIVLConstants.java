b56db80417d7b7b79662c68f7dc55fc134187018
Manchun Zheng <zmanchun@gmail.com>
2015-06-22 19:07:33 +0000

    changed version to 1.1+ so as to distinguish from the released version 1.1.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2361 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
+++ src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
@@ -43,7 +43,7 @@ public class CIVLConstants {
 			File.separator + "include"), "civl");
 
 	/** The version of this release of CIVL. */
-	public final static String version = "1.1";
+	public final static String version = "1.1+";
 
 	/**
 	 * The date of this release of CIVL. Format: YYYY-MM-DD in accordance with
