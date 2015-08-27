bff96cf23a854089ddb1b1a79fc04511284cdc79
Ziqing Luo <ziqing@udel.edu>
2015-08-27 21:42:44 +0000

    update example
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2573 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/experimental/wildcard_coassert.c
+++ examples/experimental/wildcard_coassert.c
@@ -6,7 +6,7 @@ int size, rank;
 int root = 0;
 
 int main(int argc, char * argv[]) {
-  int sum;
+  int sum = 0;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -14,7 +14,7 @@ int main(int argc, char * argv[]) {
   for(int i = 0; i < 4; i++) {
     MPI_Reduce(&i, &sum, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
     if(!rank)printf("sum=%d\n", sum);
-    $mpi_coassert(MPI_COMM_WORLD, (root + 1 - 1)@sum == (i*size));
+    $mpi_coassert(MPI_COMM_WORLD, sum@(root + 1 - 1) == (i*size));
   }
   MPI_Finalize();
   return 0;
