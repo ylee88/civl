70cbca0f42ca9a96f8cecd97b0d94798c6dfffe8
Ziqing Luo <ziqing@udel.edu>
2015-09-02 14:40:06 +0000

    add a new contract test
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2587 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ test/regress/edu/udel/cis/vsl/civl/ContractTest.java
@@ -0,0 +1,38 @@
+package edu.udel.cis.vsl.civl;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import edu.udel.cis.vsl.civl.run.IF.UserInterface;
+
+public class ContractTest {
+	/* *************************** Static Fields *************************** */
+
+	private static UserInterface ui = new UserInterface();
+
+	private static File rootDir = new File(new File("examples"), "contracts");
+
+	private static String filename(String name) {
+		return new File(rootDir, name).getPath();
+	}
+
+	@Test
+	public void collective_assert() {
+		assertFalse(ui.run(
+				"verify -enablePrintf=false -input_mpi_nprocs=3 -mpiContract",
+				filename("wildcard_coassert_bad.c")));
+		assertFalse(ui
+				.run("verify -enablePrintf=false -input_mpi_nprocs=4 -deadlock=potential -mpiContract",
+						filename("wildcard_coassert_bad.c")));
+		assertTrue(ui
+				.run("verify -enablePrintf=false -input_mpi_nprocs=4 -deadlock=potential -mpiContract",
+						filename("wildcard_coassert_barrier.c")));
+		assertTrue(ui
+				.run("verify -enablePrintf=false -input_mpi_nprocs=4 -deadlock=potential -mpiContract",
+						filename("reduce_coassert.c")));
+	}
+}
