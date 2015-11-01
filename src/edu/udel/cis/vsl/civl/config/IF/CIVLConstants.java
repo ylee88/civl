9381b334563ca6df92d76c3921e93a696578ad1c
Manchun Zheng <zmanchun@gmail.com>
2015-11-01 03:00:31 +0000

    edited version number, release date, README, manual, for release 1.5
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2738 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
+++ src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
@@ -44,13 +44,13 @@ public class CIVLConstants {
 			File.separator + "include"), "civl");
 
 	/** The version of this release of CIVL. */
-	public final static String version = "1.4+";
+	public final static String version = "1.5";
 
 	/**
 	 * The date of this release of CIVL. Format: YYYY-MM-DD in accordance with
 	 * ISO 8601.
 	 */
-	public final static String date = "2015-08-11";
+	public final static String date = "2015-10-31";
 
 	/**
 	 * The prefix of the full name of the class of a library enabler/executor.
