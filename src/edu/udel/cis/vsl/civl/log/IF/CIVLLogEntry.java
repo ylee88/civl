a80f31a428be31e409abeb19539b3e131a808148
Matthew B. Dwyer <matthewbdwyer@users.noreply.github.com>
2015-12-29 19:07:46 +0000

    Added manual entry for errorStateEquiv command line option.  Added javadoc to CIVLLogEntry and modified the equals() method to implement the same test as compareTo()
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2807 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/log/IF/CIVLLogEntry.java
+++ src/edu/udel/cis/vsl/civl/log/IF/CIVLLogEntry.java
@@ -13,9 +13,21 @@ import edu.udel.cis.vsl.gmc.GMCConfiguration;
 import edu.udel.cis.vsl.gmc.LogEntry;
 
 /**
- * This represents an entry in the error log of CIVL.
+ * This represents an entry in the error log of CIVL.   LogEntry's are stored in a
+ * collection that is ordered by the compareTo method and where the equals method
+ * determines redundancy in terms of error reports.
+ * 
+ * Currently these two methods consult the CIVLConfiguration to determine what equivalence
+ * should be applied to error states.   States that do not share the same problem kind
+ * are always inequivalent.   There are three alternative criteria that are applied:
+ *     LOC : require error locations in each proc to be the same for equivalence
+ *     CALLSTACK : require call stacks in each proc to be the same for equivalence
+ *     FULL : require the full trace, as expressed by the path condition, to be the same for equivalence
+ * 
+ * Additional equivalences are easy to implement by accessing components of the state.
  * 
  * @author zirkel
+ * @author dwyer
  * 
  */
 public class CIVLLogEntry extends LogEntry {
@@ -105,20 +117,42 @@ public class CIVLLogEntry extends LogEntry {
 			if (!problem.kind().equals(thatProblem.kind()))
 				return false;
 			else {
-				CIVLSource source1 = problem.getSource(), source2 = thatProblem
-						.getSource();
+				CIVLSource source1 = problem.getSource();
+				CIVLSource source2 = thatProblem.getSource();
+				State errorState1 = problem.state();
+				State errorState2 = thatProblem.state();
 
-				if (source1 != null && source2 != null) {
-					if (source1.equals(source2))
-						return true;
-					else
-						return source1.toString().equals(source2.toString());
-				} else if (source1 == null && source2 != null)
+				if (source1 == null && source2 != null) {
 					return false;
-				else if (source1 != null & source2 == null)
+				} else if (source1 != null & source2 == null) {
 					return false;
-				else
-					return true;
+				} else {
+					if (civlConfig.errorStateEquiv() == ErrorStateEquivalence.LOC) {
+						if (source1.equals(source2))
+							return true;
+						else
+							return source1.toString().equals(source2.toString());
+						
+					} else if (civlConfig.errorStateEquiv() == ErrorStateEquivalence.CALLSTACK) {
+						// compare based on the call stack
+						String callString1 = errorState1.callStackToString().toString();
+						String callString2 = errorState2.callStackToString().toString();
+						
+						return callString1.equals(callString2);
+						
+					} else if (civlConfig.errorStateEquiv() == ErrorStateEquivalence.FULL) {
+						// compare based on the full state
+						assert errorState1.getCanonicId() != -1 : "Expected error state to be canonic";
+						assert errorState2.getCanonicId() != -1 : "Expected error state to be canonic";
+						
+						String stateString1 = errorState1.getPathCondition().toString();
+						String stateString2 = errorState2.getPathCondition().toString();
+						
+						return stateString1.equals(stateString2);
+					} else {
+						assert false : "Invalid error state equivalence";
+					}
+				}
 			}
 		}
 		return false;
