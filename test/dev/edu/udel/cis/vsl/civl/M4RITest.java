e8ec8ddb4062eeef95db42a03c6f5f11ca84ca5b
Manchun Zheng <zmanchun@gmail.com>
2016-01-06 04:12:11 +0000

    added simple fortran tests and examples.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2825 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/dev/edu/udel/cis/vsl/civl/M4RITest.java
+++ test/dev/edu/udel/cis/vsl/civl/M4RITest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.udel.cis.vsl.civl.run.IF.UserInterface;
@@ -13,7 +12,8 @@ public class M4RITest {
 
 	/* *************************** Static Fields *************************** */
 
-	private static File rootDir = new File(new File(new File("examples"), "omp"), "m4ri");
+	private static File rootDir = new File(
+			new File(new File("examples"), "omp"), "m4ri");
 
 	private static UserInterface ui = new UserInterface();
 
@@ -31,25 +31,18 @@ public class M4RITest {
 				"-sysIncludePath=examples/omp/m4ri/m4ri:examples/omp/m4ri",
 				filename("tests/test_colswap.c"),
 				filename("m4ri/brilliantrussian.c"),
-				filename("m4ri/debug_dump.c"),
-				filename("m4ri/djb.c"),
-				filename("m4ri/echelonform.c"),
-				filename("m4ri/graycode.c"),
-				filename("m4ri/io.c"),
-				filename("m4ri/misc.c"),
-				filename("m4ri/mmc.c"),
-				filename("m4ri/mp.c"),
-				filename("m4ri/mzd.c"),
-				filename("m4ri/mzp.c"),
-				filename("m4ri/ple_russian.c"),
-				filename("m4ri/ple.c"),
-				filename("m4ri/solve.c"),
-				filename("m4ri/strassen.c"),
+				filename("m4ri/debug_dump.c"), filename("m4ri/djb.c"),
+				filename("m4ri/echelonform.c"), filename("m4ri/graycode.c"),
+				filename("m4ri/io.c"), filename("m4ri/misc.c"),
+				filename("m4ri/mmc.c"), filename("m4ri/mp.c"),
+				filename("m4ri/mzd.c"), filename("m4ri/mzp.c"),
+				filename("m4ri/ple_russian.c"), filename("m4ri/ple.c"),
+				filename("m4ri/solve.c"), filename("m4ri/strassen.c"),
 				filename("m4ri/triangular_russian.c"),
 				filename("m4ri/triangular.c")));
-		
+
 	}
-	
+
 	@Test
 	public void m4riVerify() {
 		assertTrue(ui.run("verify", "-ompNoSimplify",
@@ -57,24 +50,16 @@ public class M4RITest {
 				"-sysIncludePath=examples/omp/m4ri/m4ri:examples/omp/m4ri",
 				filename("tests/test_colswap.c"),
 				filename("m4ri/brilliantrussian.c"),
-				filename("m4ri/debug_dump.c"),
-				filename("m4ri/djb.c"),
-				filename("m4ri/echelonform.c"),
-				filename("m4ri/graycode.c"),
-				filename("m4ri/io.c"),
-				filename("m4ri/misc.c"),
-				filename("m4ri/mmc.c"),
-				filename("m4ri/mp.c"),
-				filename("m4ri/mzd.c"),
-				filename("m4ri/mzp.c"),
-				filename("m4ri/ple_russian.c"),
-				filename("m4ri/ple.c"),
-				filename("m4ri/solve.c"),
-				filename("m4ri/strassen.c"),
+				filename("m4ri/debug_dump.c"), filename("m4ri/djb.c"),
+				filename("m4ri/echelonform.c"), filename("m4ri/graycode.c"),
+				filename("m4ri/io.c"), filename("m4ri/misc.c"),
+				filename("m4ri/mmc.c"), filename("m4ri/mp.c"),
+				filename("m4ri/mzd.c"), filename("m4ri/mzp.c"),
+				filename("m4ri/ple_russian.c"), filename("m4ri/ple.c"),
+				filename("m4ri/solve.c"), filename("m4ri/strassen.c"),
 				filename("m4ri/triangular_russian.c"),
 				filename("m4ri/triangular.c")));
-		
+
 	}
-	
-	
+
 }
