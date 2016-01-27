6ba2218d0e679806855961b1c7e0dcc2a1d3a3e4
Yihao Yan <yihaoyan1@gmail.com>
2016-01-27 20:16:14 +0000

    add -quiet option
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2895 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ test/regress/edu/udel/cis/vsl/civl/TestConstants.java
@@ -0,0 +1,31 @@
+package edu.udel.cis.vsl.civl;
+
+/**
+ * 
+ * This class contains all the constants used in test files
+ * 
+ *
+ */
+public class TestConstants {
+	
+	/**
+	 * commands
+	 */
+	public static String RUN = "run";
+	
+	public static String VERIFY = "verify";
+	
+	public static String REPLAY = "replay";
+	
+	public static String COMPARE = "compare";
+	
+	/**
+	 * options
+	 */
+	public static String QUIET = "-quiet";
+	
+	public static String IMPL = "-impl";
+	
+	public static String SPEC = "-spec";
+	
+}
