b346e96c43ba24c462b39caf2063f775864f1b40
Yihao Yan <yihaoyan1@gmail.com>
2016-01-27 20:16:00 +0000

    add -quiet option
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2894 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ test/regress/edu/udel/cis/vsl/civl/QuietOptionTest.java
@@ -0,0 +1,63 @@
+package edu.udel.cis.vsl.civl;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import java.io.File;
+import org.junit.Test;
+import edu.udel.cis.vsl.civl.run.IF.UserInterface;
+
+/**
+ * Test cases used to test -quiet option in the command
+ * eg. verify -quiet filename
+ * 
+ * @author yihaoyan
+ *
+ */
+public class QuietOptionTest {
+	
+	private static File rootDir1 = new File(new File("examples"), "concurrency");
+	private static File rootDir2 = new File(new File("examples"), "compare/adder");
+	private static UserInterface ui = new UserInterface();
+	private static String filename1(String name) {
+		return new File(rootDir1, name).getPath();
+	}
+	private static String filename2(String name) {
+		return new File(rootDir2, name).getPath();
+	}
+	
+	@Test
+	public void adderRun(){
+		assertTrue(ui.run(TestConstants.RUN, "-inputB=5", TestConstants.QUIET,
+				filename1("adder.cvl")));
+	}
+	
+	@Test
+	public void adderVerify(){
+		assertTrue(ui.run(TestConstants.VERIFY, "-inputB=5", TestConstants.QUIET,
+				filename1("adder.cvl")));
+	}
+	
+	@Test
+	public void adderBadVerify(){
+		assertFalse(ui.run(TestConstants.VERIFY, "-inputB=5", TestConstants.QUIET,
+				filename1("adderBad.cvl")));
+	}
+	
+	@Test
+	public void adderBadReplay(){
+		assertFalse(ui.run(TestConstants.VERIFY, "-inputB=5", TestConstants.QUIET,
+				filename1("adderBad.cvl")));
+		
+		assertFalse(ui.run(TestConstants.REPLAY, TestConstants.QUIET,
+				filename1("adderBad.cvl")));
+	}
+	
+	@Test
+	public void adderCompare(){
+		assertTrue(ui.run(TestConstants.COMPARE, "-inputNB=4", "-inputNPROCSB=2",
+				TestConstants.QUIET, TestConstants.IMPL,
+				filename2("adder_par.cvl"), TestConstants.SPEC,
+				filename2("adder_spec.cvl")));
+	}
+	
+}
