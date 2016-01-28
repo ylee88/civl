ecd2468749151e84926bf00bb40272d5bb64a70f
Yihao Yan <yihaoyan1@gmail.com>
2016-01-28 20:13:28 +0000

    problem1 update
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2900 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/experimental/problem1.c
+++ examples/experimental/problem1.c
@@ -1,27 +1,8 @@
 #include <civlc.cvh>
-#include <stdio.h>
 
-$input int A_BOUND;
-$input int B_BOUND;
-$input int A;
-$input int B;
-$assume (A>0 && B>0 && A<A_BOUND && B<B_BOUND);
-/*
-command: civl verify -inputA_BOUND=4 -inputB_BOUND=6 problem1.c
-*/
-int myGCD(int a, int b){
-  while(a != b){
-    if(a > b)
-      a = a-b;
-    if(b > a)
-      b = b-a;
-  }
-  return a;
+$input int A,B;
+
+int main(){
+  $assert($forall {i = 2 .. B } (A%i != 0));
 }
 
-void main(){
-  int result1 = myGCD(A, B);
-  int minAB = A < B ? A : B;
-  $assert($forall {i = (result1+1) .. (minAB)} (A%i != 0 || B%i != 0));
-  $assert( A%result1 == 0 && B%result1 == 0);
-}
