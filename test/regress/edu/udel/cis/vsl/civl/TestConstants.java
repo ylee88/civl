7857082be8ffca3ceae2502ed9005472207a5ac7
Manchun Zheng <zmanchun@gmail.com>
2016-02-01 20:50:59 +0000

    added NOPRINTF constant
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@2914 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/regress/edu/udel/cis/vsl/civl/TestConstants.java
+++ test/regress/edu/udel/cis/vsl/civl/TestConstants.java
@@ -7,25 +7,27 @@ package edu.udel.cis.vsl.civl;
  *
  */
 public class TestConstants {
-	
+
 	/**
 	 * commands
 	 */
 	public static String RUN = "run";
-	
+
 	public static String VERIFY = "verify";
-	
+
 	public static String REPLAY = "replay";
-	
+
 	public static String COMPARE = "compare";
-	
+
 	/**
 	 * options
 	 */
 	public static String QUIET = "-quiet";
-	
+
 	public static String IMPL = "-impl";
-	
+
 	public static String SPEC = "-spec";
-	
+
+	public static String NOPRINTF = "-enablePrintf=false";
+
 }
