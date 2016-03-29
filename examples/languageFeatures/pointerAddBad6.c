7c73e9d318ef44a9b7493dc3f2739025825805e7
Stephen Siegel <siegel@udel.edu>
2016-03-29 17:17:42 +0000

    Small simple cleanup of example code.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3056 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/languageFeatures/pointerAddBad6.c
+++ examples/languageFeatures/pointerAddBad6.c
@@ -2,10 +2,10 @@
 #include <civlc.cvh>
 $input int offset;
 $assume(0<=offset && offset<=1);
-int main(int argc, char * argv[]) {
+int main() {
   int a = 8;
   void *p = &a + 1;
-  void * q = p + offset;
+  void *q = p + offset;
 
   return 0;
 }
