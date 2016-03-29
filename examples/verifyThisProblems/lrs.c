724ceb255550868cad2c32c7f16c9d731f92421a
Yihao Yan <yihaoyan1@gmail.com>
2016-03-29 21:28:13 +0000

    add comment for verifyThis problem
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3063 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/verifyThisProblems/lrs.c
+++ examples/verifyThisProblems/lrs.c
@@ -1,3 +1,29 @@
+/*
+This is a problem in 2012 as an advance problem for LCP
+
+Here is the description:
+Together with a suffix array, LCP can be used to solve interesting text
+problems, such as finding the longest repeated substring (LRS) in a text.
+
+A suffix array (for a given text) is an array of all suffixes of the
+text. For the text [7,8,8,6], the suffix array is
+[[7,8,8,6],
+   [8,8,6],
+     [8,6],
+       [6]]
+
+Typically, the suffixes are not stored explicitly as above but
+represented as pointers into the original text. The suffixes in a suffix
+array  are sorted in lexicographical order. This way, occurrences of
+repeated substrings in the original text are neighbors in the suffix
+array. 
+
+For the above, example (assuming pointers are 0-based integers), the
+sorted suffix array is:
+
+[3,0,2,1]
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <civlc.cvh>
