d332e9f384613573b3ae724b457dbd79e65b3758
Manchun Zheng <zmanchun@gmail.com>
2016-05-12 14:17:00 +0000

    added test for powwow meeting
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3252 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ test/dev/edu/udel/cis/vsl/civl/PowwowTest.java
@@ -0,0 +1,34 @@
+package edu.udel.cis.vsl.civl;
+
+import static edu.udel.cis.vsl.civl.TestConstants.VERIFY;
+import static org.junit.Assert.assertFalse;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import edu.udel.cis.vsl.civl.run.IF.UserInterface;
+
+public class PowwowTest {
+	/* *************************** Static Fields *************************** */
+
+	private static UserInterface ui = new UserInterface();
+
+	private static File rootDir = new File(new File("examples"), "powwow");
+
+	private static String filename(String name) {
+		return new File(rootDir, name).getPath();
+	}
+
+	@Test
+	public void assert1() {
+		assertFalse(ui.run(VERIFY, filename("assertFalse.c")));
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		ui = null;
+		rootDir = null;
+	}
+}
