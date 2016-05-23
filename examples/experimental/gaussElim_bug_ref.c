cec6b1f11fcd5ff6be06d823af5c94687bd557eb
Stephen Siegel <siegel@udel.edu>
2016-05-23 16:51:40 +0000

    Cleaned up two examples.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3294 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/experimental/gaussElim_bug_ref.c
+++ examples/experimental/gaussElim_bug_ref.c
@@ -1,38 +1,12 @@
 #include <civlc.cvh>
-#include <assert.h>
 
-$input double data[3][3];
-$input double A,B,C,D,E,F,G,H,I;
-//00 => A
-//01 => B
-//02 => C
-//10 => D
-//11 => E
-//12 => F
-//20 => G
-//21 => H
-//22 => I
+$input double A,B,C,E,F,G,H,I;
 
-$assume (0==(H * A - 1 * G * B));
-$assume (0 == D);
-$assume (0 != ((I * A) - (G * C)));
-$assume (0 != E &&
-	 0 != G &&
-	 0 != A);
-$assume ((0 == (E * I * A 
-		- E * G * C 
-		- F * H * A
-		+ F * G * B)));
+$assume (0 == H*A - G*B);
+$assume (0 != I*A - G*C);
+$assume (0 != E && 0 != G && 0 != A);
+$assume (0 == E*I*A - E*G*C - F*H*A	+ F*G*B);
 
 int main() {
-  $assert( 1 == 0, "unreachable");
+  $assert(0, "unreachable");
 }
-
-
-
-
-
-
-
-
-
