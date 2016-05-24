54a431fbdcca199386ef980f1c692b5d52a8daa2
Yihao Yan <yihaoyan1@gmail.com>
2016-05-24 20:01:05 +0000

    remove CommonConstatnts
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3305 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/util/IF/CommonConstants.java
+++
@@ -1,9 +0,0 @@
-package edu.udel.cis.vsl.civl.util.IF;
-
-public class CommonConstants {
-	public static String current_command_line = null;
-
-	public static void setCurrentCommandLine(String currCommandLine){
-		current_command_line = currCommandLine;
-	}
-}
