a4384c1a08e7d10ce5b69c5ceb8054682b99a634
Yihao Yan <yihaoyan1@gmail.com>
2016-05-24 19:16:55 +0000

    add CommonConstants.java
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3301 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ src/edu/udel/cis/vsl/civl/util/IF/CommonConstants.java
@@ -0,0 +1,9 @@
+package edu.udel.cis.vsl.civl.util.IF;
+
+public class CommonConstants {
+	public static String current_command_line = null;
+
+	public static void setCurrentCommandLine(String currCommandLine){
+		current_command_line = currCommandLine;
+	}
+}
