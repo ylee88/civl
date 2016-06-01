66f00bb07553d6485b5cbe173f977120cd31dc7a
Manchun Zheng <zmanchun@gmail.com>
2016-06-01 19:47:50 +0000

    added benchmarking example for guards.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3360 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ bench/edu/udel/cis/vsl/civl/bench/BinaryGuardsBenchmark.java
@@ -0,0 +1,27 @@
+package edu.udel.cis.vsl.civl.bench;
+
+import edu.udel.cis.vsl.civl.run.IF.UserInterface;
+
+/**
+ * Benchmark of the adder example. Execution time should be within 20 to 58
+ * seconds.
+ * 
+ * @author zmanchun
+ * 
+ */
+public class BinaryGuardsBenchmark {
+	private static UserInterface ui = new UserInterface();
+
+	public static void main(String[] args) {
+		// -inputB=7: 12 seconds
+		// -inputB=8: 39 seconds
+		String civlDir = ".";
+
+		if (args.length > 0)
+			civlDir = args[0];
+		System.out.println(">>>>>>>> binary guards <<<<<<<<");
+		ui.run("verify -inputN=1000 " + civlDir
+				+ "/examples/bench/binaryGuards.cvl");
+	}
+
+}
