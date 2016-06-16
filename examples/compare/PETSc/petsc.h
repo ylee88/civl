0f53e8a4083dccdd68033b06d998bcdb0f93cd6b
Si Li <sili@udel.edu>
2016-06-16 19:19:26 +0000

    minor change
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3389 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/compare/PETSc/petsc.h
+++ examples/compare/PETSc/petsc.h
@@ -1,6 +1,7 @@
 /*
- * self-defined petsc.h header for provesa examples: ex2a.c, ex2b.c,ex2c.c,ex2d.c.
+ * Self-defined petsc.h header for provesa examples: ex2a.c, ex2b.c,ex2c.c,ex2d.c.
  * https://repo.anl-external.org/repos/provesa/codes/mxm/
+ * Full example from PETSc website: http://www.mcs.anl.gov/petsc/petsc-current/src/snes/examples/tutorials/ex5.c.html
  */
 
 #ifndef _PETSC_
@@ -36,13 +37,13 @@ typedef double PetscReal;
 typedef double PetscScalar;
 
 /* C struct that contains information about a structured grid and a processors logical location in it. */
-typedef struct DMDALocalInfo {
-  // PetscInt         dim,dof,sw;
+typedef struct DMDALocalInfo { /* using distributed arrays */
+  PetscInt         dim,dof,sw;
   PetscInt         mx,my,mz;    /* global number of grid points in each direction */
   PetscInt         xs,ys,zs;    /* starting point of this processor, excluding ghosts */
   PetscInt         xm,ym,zm;    /* number of grid points on this processor, excluding ghosts */
-  //PetscInt         gxs,gys,gzs;    /* starting point of this processor including ghosts */
-  //PetscInt         gxm,gym,gzm;    /* number of grid points on this processor including ghosts */
+  PetscInt         gxs,gys,gzs;    /* starting point of this processor including ghosts */
+  PetscInt         gxm,gym,gzm;    /* number of grid points on this processor including ghosts */
 } DMDALocalInfo;
 
 /* ----------Functions---------- */
@@ -57,10 +58,10 @@ typedef struct DMDALocalInfo {
 #define PetscFunctionReturn(x)    return(x)
 
 /* Adds floating point operations to the global counter. */
-PetscErrorCode PetscLogFlops(double );
+PetscErrorCode PetscLogFlops(double ); //{return 0;}
 
 /* Checks error code, if non-zero it calls the error handler and then returns */
-PetscErrorCode CHKERRQ(PetscErrorCode );
+PetscErrorCode CHKERRQ(PetscErrorCode ); // {return 0;}
 
 /*
 #undef __FUNCT__
@@ -77,3 +78,25 @@ PETSC_STATIC_INLINE PetscErrorCode PetscLogFlops(PetscLogDouble n)
 */
 
 #endif
+
+/******************************************************************************************/
+/* about SFI Bratu equation */
+/* petsc-2.0.28/src/snes/examples/tutorial/ex5f.F
+ *
+ *  command: mpirun -np <procs> ex5f [-help] [all PETSc options]
+ *  -par <param>： SFI parameter lambda ：0 <= lambda <= 6.81；
+    -mx <xg> 
+    -my <yg>
+    -Nx <npx>
+    -Ny <npy>
+ */
+
+/*   static char help[] = "Bratu nonlinear PDE in 2d.\n			\
+  3: We solve the  Bratu (SFI - solid fuel ignition) problem in a 2D rectangular\n\
+  4: domain, using distributed arrays (DMDAs) to partition the parallel grid.\n\
+  5: The command line options include:\n\
+  6:   -par <parameter>, where <parameter> indicates the problem's nonlinearity\n\
+  7:      problem SFI:  <parameter> = Bratu parameter (0 <= par <= 6.81)\n\n\
+  8:   -m_par/n_par <parameter>, where <parameter> indicates an integer\n \
+  9:       that MMS3 will be evaluated with 2^m_par, 2^n_par";
+*/
