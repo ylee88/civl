96f1f4cad92964f900c2cebe38579537b113fff5
Si Li <sili@udel.edu>
2016-06-16 19:20:45 +0000

    minor change
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3391 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/compare/PETSc/ex2Driver.c
+++ examples/compare/PETSc/ex2Driver.c
@@ -1,5 +1,6 @@
 /* PETSc example driver for ex2a.c, ex2b.c,ex2c.c,ex2d.c,
  * https://repo.anl-external.org/repos/provesa/codes/mxm/
+ * command: civl verify ex2Driver.c ex2a.c
  */
 
 #include <civlc.cvh>
@@ -11,29 +12,33 @@ typedef struct {
    PassiveReal param;          /* test problem parameter */
 } AppCtx;
 
-$input PetscScalar x_data[5][5];
-$input PetscScalar f_data[5][5];
-$input AppCtx user[5];
-$input DMDALocalInfo info[5];
+#define M 5
+#define N 5
+#define K 3
+#define L 3
+$input PetscScalar x_data[M][N];
+$input PetscScalar f_data[M][N];
+$input AppCtx user[K];
+$input DMDALocalInfo info[L];
 
 PetscErrorCode FormFunctionLocal(DMDALocalInfo *,PetscScalar **,PetscScalar **,AppCtx *);
 
 int main() {
   PetscScalar **x, **f;
 
-  x = (double **)malloc(5*sizeof(double *));
-    for (int i = 0; i < 5 ; i++) {
-      x[i] = (double *)malloc(5*sizeof(double));
+  x = (double **)malloc(M*sizeof(double *));
+    for (int i = 0; i < M ; i++) {
+      x[i] = (double *)malloc(N*sizeof(double));
       //x[i] = $havoc(&x[i]) 
     }
 
-  f = (double **)malloc(5*sizeof(double *));
-    for (int i = 0; i < 5 ; i++) {
-      f[i] = (double *)malloc(5*sizeof(double));
+  f = (double **)malloc(M*sizeof(double *));
+    for (int i = 0; i < M ; i++) {
+      f[i] = (double *)malloc(N*sizeof(double));
     }
 
-  for (int i = 0; i < 5 ; i++)
-    for (int j = 0; j < 5 ; j++) {
+  for (int i = 0; i < M ; i++)
+    for (int j = 0; j < N ; j++) {
       x[i][j] = x_data[i][j];
       f[i][j] = f_data[i][j];
     }
