ad7fb7d9f56ed63f918e79123f378ca171423710
Manchun Zheng <zmanchun@gmail.com>
2016-07-08 17:45:02 +0000

    ignore large test
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3424 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/regress/edu/udel/cis/vsl/civl/transform/CompareTest.java
+++ test/regress/edu/udel/cis/vsl/civl/transform/CompareTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 
 import org.junit.AfterClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.udel.cis.vsl.civl.TestConstants;
@@ -35,68 +36,84 @@ public class CompareTest {
 
 	@Test
 	public void sumN() {
-		assertTrue(ui.run(COMPARE, "-inputN=10", QUIET, SPEC, filename("sum", "sumNspec.cvl"), IMPL,
+		assertTrue(ui.run(COMPARE, "-inputN=10", SPEC,
+				filename("sum", "sumNspec.cvl"), IMPL,
 				filename("sum", "sumNimpl.cvl")));
 	}
 
 	@Test
 	public void adder() {
-		assertTrue(ui.run(COMPARE, QUIET, NO_PRINTF, "-input_mpi_nprocs_hi=2", "-inputNB=4", IMPL,
-				filename("adder", "adder_par.c"), SPEC, filename("adder", "adder_spec.c")));
+		assertTrue(ui.run(COMPARE, QUIET, NO_PRINTF, "-input_mpi_nprocs_hi=2",
+				"-inputNB=4", IMPL, filename("adder", "adder_par.c"), SPEC,
+				filename("adder", "adder_spec.c")));
 	}
 
 	@Test
 	public void max() {
-		assertFalse(ui.run(COMPARE, QUIET, NO_PRINTF, "-inputB=4 -min -spec", filename("max", "max.cvl"),
-				filename("max", "max_seq.cvl"), "-impl -inputNPROCS=2 -inputBLOCK_SIZE=2", filename("max", "max.cvl"),
+		assertFalse(ui.run(COMPARE, QUIET, NO_PRINTF, "-inputB=4 -min -spec",
+				filename("max", "max.cvl"), filename("max", "max_seq.cvl"),
+				"-impl -inputNPROCS=2 -inputBLOCK_SIZE=2",
+				filename("max", "max.cvl"), filename("max", "max_par.cvl")));
+		assertFalse(ui.run(REPLAY, QUIET, NO_PRINTF, "-spec",
+				filename("max", "max.cvl"), filename("max", "max_seq.cvl"),
+				"-impl", filename("max", "max.cvl"),
 				filename("max", "max_par.cvl")));
-		assertFalse(ui.run(REPLAY, QUIET, NO_PRINTF, "-spec", filename("max", "max.cvl"),
-				filename("max", "max_seq.cvl"), "-impl", filename("max", "max.cvl"), filename("max", "max_par.cvl")));
 	}
 
 	@Test
 	public void dotMpiPthreads() {
-		assertTrue(ui.run(COMPARE, QUIET, "-inputVECLEN=5", SPEC, "-inputMAXTHRDS=2",
-				filename("dot", "mpithreads_threads.c"), IMPL, "-input_mpi_nprocs=2",
+		assertTrue(ui.run(COMPARE, QUIET, "-inputVECLEN=5", SPEC,
+				"-inputMAXTHRDS=2", filename("dot", "mpithreads_threads.c"),
+				IMPL, "-input_mpi_nprocs=2",
 				filename("dot", "mpithreads_mpi.c")));
 	}
 
+	@Ignore
 	@Test
 	public void dotHybrid() {
-		assertTrue(ui.run(TestConstants.VERIFY, QUIET, "-inputVECLEN=1 -input_mpi_nprocs=2 -inputMAXTHRDS=2",
+		assertTrue(ui.run(TestConstants.VERIFY, QUIET,
+				"-inputVECLEN=1 -input_mpi_nprocs=2 -inputMAXTHRDS=2",
 				filename("dot", "mpithreads_both.c")));
 	}
 
 	@Test
 	public void dotMpiSerial() {
-		assertFalse(ui.run(COMPARE, NO_PRINTF, QUIET, "-inputVECLEN=5", SPEC, filename("dot", "mpithreads_serial.c"),
-				IMPL, "-input_mpi_nprocs=2", filename("dot", "mpithreads_mpi.c")));
+		assertFalse(ui.run(COMPARE, NO_PRINTF, QUIET, "-inputVECLEN=5", SPEC,
+				filename("dot", "mpithreads_serial.c"), IMPL,
+				"-input_mpi_nprocs=2", filename("dot", "mpithreads_mpi.c")));
 
-		assertFalse(ui.run(REPLAY, QUIET, NO_PRINTF, SPEC, filename("dot", "mpithreads_serial.c"), IMPL,
+		assertFalse(ui.run(REPLAY, QUIET, NO_PRINTF, SPEC,
+				filename("dot", "mpithreads_serial.c"), IMPL,
 				"-input_mpi_nprocs=2", filename("dot", "mpithreads_mpi.c")));
 	}
 
 	@Test
 	public void dotPthreadsSerial() {
-		assertFalse(ui.run(COMPARE, NO_PRINTF, QUIET, "-inputVECLEN=5", SPEC, filename("dot", "mpithreads_serial.c"),
-				IMPL, "-inputMAXTHRDS=2", filename("dot", "mpithreads_threads.c")));
+		assertFalse(ui.run(COMPARE, NO_PRINTF, QUIET, "-inputVECLEN=5", SPEC,
+				filename("dot", "mpithreads_serial.c"), IMPL,
+				"-inputMAXTHRDS=2", filename("dot", "mpithreads_threads.c")));
 	}
 
 	@Test
 	public void dotHybridSerial() {
-		assertFalse(ui.run(COMPARE, QUIET, "-inputVECLEN=5", SPEC, filename("dot", "mpithreads_serial.c"), IMPL,
-				"-input_mpi_nprocs=2 -inputMAXTHRDS=2", filename("dot", "mpithreads_both.c")));
+		assertFalse(ui.run(COMPARE, QUIET, "-inputVECLEN=5", SPEC,
+				filename("dot", "mpithreads_serial.c"), IMPL,
+				"-input_mpi_nprocs=2 -inputMAXTHRDS=2",
+				filename("dot", "mpithreads_both.c")));
 	}
 
 	@Test
 	public void dotMpiHybrid() {
-		assertFalse(ui.run(COMPARE, QUIET, "-inputVECLEN=4 -input_mpi_nprocs=2", SPEC,
-				filename("dot", "mpithreads_mpi.c"), IMPL, "-inputMAXTHRDS=2", filename("dot", "mpithreads_both.c")));
+		assertFalse(ui.run(COMPARE, QUIET,
+				"-inputVECLEN=4 -input_mpi_nprocs=2", SPEC,
+				filename("dot", "mpithreads_mpi.c"), IMPL, "-inputMAXTHRDS=2",
+				filename("dot", "mpithreads_both.c")));
 	}
 
 	@Test
 	public void outputfiles() {
-		assertTrue(ui.run(COMPARE, QUIET, SPEC, filename("io", "out1.c"), IMPL, filename("io", "out2.c")));
+		assertTrue(ui.run(COMPARE, QUIET, SPEC, filename("io", "out1.c"), IMPL,
+				filename("io", "out2.c")));
 	}
 
 	@AfterClass
