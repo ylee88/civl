9335eca3962f1d6eb923dda3b5e22f8b8219eef3
Si Li <sili@udel.edu>
2016-07-28 19:35:13 +0000

    clean file
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3502 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/compare/PETSc/petsc.h
+++ examples/compare/PETSc/petsc.h
@@ -1,7 +1,12 @@
 /*
- * Self-defined petsc.h header for provesa examples: ex2a.c, ex2b.c,ex2c.c,ex2d.c.
- * https://repo.anl-external.org/repos/provesa/codes/mxm/
- * Full example from PETSc website: http://www.mcs.anl.gov/petsc/petsc-current/src/snes/examples/tutorials/ex5.c.html
+ * Self-defined petsc.h header for provesa PETSc examples: ex2a.c, ex2b.c,
+ * ex2c.c,ex2d.c. This header is used for verifying the equivalence of those
+ * examples by using CIVL. This header is made based on the functions and
+ * types which is used in those PETSc examples. The definition of types and 
+ * funcitons comes from PETSc home page: https://www.mcs.anl.gov/petsc/
+ * PETSc examples: https://repo.anl-external.org/repos/provesa/codes/mxm/
+ *
+ * Author: Si Li <sili@udel.edu>
  */
 
 #ifndef _PETSC_
@@ -12,55 +17,63 @@
 
 /* -------Types------- */
 
-/* PetscErrorCode - datatype used for return error code from almost all PETSc functions */
+/* PetscErrorCode - datatype used for return error code from almost
+ * all PETSc functions */
 typedef int PetscErrorCode;
 
 /* PETSc type that represents a PetscReal.
- * This is the same as a PetscReal except in code that is automatically differentiated it 
- * is treated as a constant (not an indendent or dependent variable) 
+ * This is the same as a PetscReal except in code that is automatically
+ * differentiated it is treated as a constant (not an indendent or 
+ * dependent variable) 
  */
 typedef double PassiveReal;
 
-/* PETSc type that represents integer - used primarily to represent size of arrays and indexing
- * into arrays. Its size can be * configured with the option-with-64-bit-indices - to be either 
+/* PETSc type that represents integer - used primarily to represent size
+ * of arrays and indexing into arrays. Its size can be * configured with
+ * the option-with-64-bit-indices - to be either 
  * 32bit or 64bit [default 32 bit ints]
  */
 typedef int PetscInt;
 
-/* PetscReal - PETSc type that represents a real number version of PetscScalar */
+/* PetscReal - PETSc type that represents a real number version of
+ * PetscScalar */
 typedef double PetscReal;
 
-/* PetscScalar - PETSc type that represents either a double precision real number, a double precision
- * complex number, a single precision real number, a long double or an int - if the code is configured
- * with --with-scalar-type=real, complex --with-precision=single,double,__float128
+/* PetscScalar - PETSc type that represents either a double precision
+ * real number, a double precision complex number, a single precision
+ * real number, a long double or an int - if the code is configured
+ * with --with-scalar-type=real, complex --with-precision=single,
+ * double,__float128
  */
 typedef double PetscScalar;
 
-/* C struct that contains information about a structured grid and a processors logical location in it. */
+/* C struct that contains information about a structured grid and
+ a processors logical location in it. */
 typedef struct DMDALocalInfo { /* using distributed arrays */
-  PetscInt         dim,dof,sw;
-  PetscInt         mx,my,mz;    /* global number of grid points in each direction */
-  PetscInt         xs,ys,zs;    /* starting point of this processor, excluding ghosts */
-  PetscInt         xm,ym,zm;    /* number of grid points on this processor, excluding ghosts */
-  PetscInt         gxs,gys,gzs;    /* starting point of this processor including ghosts */
-  PetscInt         gxm,gym,gzm;    /* number of grid points on this processor including ghosts */
+  PetscInt dim,dof,sw;
+  PetscInt mx,my,mz;    /* global number of grid points in each direction */
+  PetscInt xs,ys,zs;    /* starting point of this processor, excluding ghosts */
+  PetscInt xm,ym,zm;    /* number of grid points on this processor, excluding ghosts */
+  PetscInt gxs,gys,gzs; /* starting point of this processor including ghosts */
+  PetscInt gxm,gym,gzm; /* number of grid points on this processor including ghosts */
 } DMDALocalInfo;
 
 /* ----------Functions---------- */
 
-/* First executable line of each PETSc function, used for error handling. Final line of PETSc 
- * functions should be PetscFunctionReturn(0);
+/* First executable line of each PETSc function, used for error handling.
+ * Final line of PETSc functions should be PetscFunctionReturn(0);
  */
 #define PetscFunctionBegin
 
-/* Last executable line of each PETSc function used for error handling. Replaces return() */
-//void PetscFunctionReturn(0);
+/* Last executable line of each PETSc function used for error handling.
+ * Replaces return() */
 #define PetscFunctionReturn(x)    return(x)
 
 /* Adds floating point operations to the global counter. */
 PetscErrorCode PetscLogFlops(double ); //{return 0;}
 
-/* Checks error code, if non-zero it calls the error handler and then returns */
+/* Checks error code, if non-zero it calls the error handler and then
+ * returns */
 PetscErrorCode CHKERRQ(PetscErrorCode ); // {return 0;}
 
 /*
@@ -78,25 +91,3 @@ PETSC_STATIC_INLINE PetscErrorCode PetscLogFlops(PetscLogDouble n)
 */
 
 #endif
-
-/******************************************************************************************/
-/* about SFI Bratu equation */
-/* petsc-2.0.28/src/snes/examples/tutorial/ex5f.F
- *
- *  command: mpirun -np <procs> ex5f [-help] [all PETSc options]
- *  -par <param>： SFI parameter lambda ：0 <= lambda <= 6.81；
-    -mx <xg> 
-    -my <yg>
-    -Nx <npx>
-    -Ny <npy>
- */
-
-/*   static char help[] = "Bratu nonlinear PDE in 2d.\n			\
- *   We solve the  Bratu (SFI - solid fuel ignition) problem in a 2D rectangular\n \
- *   domain, using distributed arrays (DMDAs) to partition the parallel grid.\n \
- *   The command line options include:\n					\
- *   -par <parameter>, where <parameter> indicates the problem's nonlinearity\n\
- *   problem SFI:  <parameter> = Bratu parameter (0 <= par <= 6.81)\n\n \
- *   -m_par/n_par <parameter>, where <parameter> indicates an integer\n	\
- *   that MMS3 will be evaluated with 2^m_par, 2^n_par";
-*/
