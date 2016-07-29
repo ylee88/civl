f2eb07706acbac59013849b6e2fcdd64cfbab6e4
Matthew B. Dwyer <matthewbdwyer@users.noreply.github.com>
2016-07-29 16:03:24 +0000

    Adding more OpenMP systems
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3504 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/omp/m4ri/build.sh
+++ examples/omp/m4ri/build.sh
@@ -1,1 +1,1 @@
-civl show -showProgram -ompNoSimplify -userIncludePath=./m4ri/ -sysIncludePath=./m4ri/m4ri/ tests/test_colswap.c
+civl show -verbose -showProgram -ompNoSimplify -userIncludePath=./m4ri/ -sysIncludePath=./ 
---
+++ examples/omp/m4ri/verify.sh
@@ -0,0 +1,1 @@
+civl verify -errorBound=10 -ompNoSimplify -userIncludePath=./m4ri/ -sysIncludePath=./ tests/test_colswap.c
