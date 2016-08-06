5b6be95e415c0680faa177b38e4ad16183ee355d
Ziqing Luo <ziqing@udel.edu>
2016-08-06 20:25:02 +0000

    newer broadcast example
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3552 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/contracts/contractsMPI/broadcast.c
+++ examples/contracts/contractsMPI/broadcast.c
@@ -2,15 +2,15 @@
 #include<civlc.cvh>
 
 /*@ 
-  @ requires \valid(buf + (0 .. count));
   @ \mpi_collective(comm, P2P):
   @   requires 0 <= root && root < \mpi_comm_size;
-  @   requires \mpi_agree(root) && \mpi_agree(count);
+  @   requires \mpi_agree(root) && \mpi_agree(count * \mpi_extent(datatype));
   @   requires 0 < count && count < 10;
-  @   ensures \mpi_equals(buf, count, MPI_INT, \remote(buf, root));
+  @   requires \mpi_valid(buf, count, datatype);
+  @   ensures \mpi_equals(buf, count, datatype, \on(root, buf));
   @   waitsfor root;
   @*/
-int broadcast(int * buf, int count, 
+int broadcast(void * buf, int count, 
 	      MPI_Datatype datatype, int root, MPI_Comm comm) {
   int nprocs, rank;
   int tag = 999;
@@ -20,15 +20,9 @@ int broadcast(int * buf, int count,
   if (rank == root) {
     for (int i = 0; i < nprocs; i++)
       if (i != root)
-	MPI_Send(buf, count, MPI_INT, i, tag, comm);
+	MPI_Send(buf, count, datatype, i, tag, comm);
   } else
-    MPI_Recv(buf, count, MPI_INT, root, tag, comm,
+    MPI_Recv(buf, count, datatype, root, tag, comm,
 	     MPI_STATUS_IGNORE);
-  return 0;
-}
-
-
-int main() {
-  broadcast(NULL, 0, MPI_INT, 0, (MPI_Comm)0);
   return 0;
 }
