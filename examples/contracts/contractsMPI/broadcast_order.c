c7fc5a54efbec998adf7b7b4baea28d3fa1f0a1f
Ziqing Luo <ziqing@udel.edu>
2016-08-07 15:20:04 +0000

    add a new test
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3565 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ examples/contracts/contractsMPI/broadcast_order.c
@@ -0,0 +1,28 @@
+#include<mpi.h>
+#include<civlc.cvh>
+
+/*@ 
+  @ \mpi_collective(comm, P2P):
+  @   requires \mpi_valid(buf, count, datatype);
+  @   requires 0 <= root && root < \mpi_comm_size;
+  @   requires \mpi_agree(root) && \mpi_agree(count * \mpi_extent(datatype));
+  @   requires 0 < count && count < 10;
+  @   ensures \mpi_equals(buf, count, datatype, \on(root, buf));
+  @   waitsfor root;
+  @*/
+int broadcast(void * buf, int count, 
+	      MPI_Datatype datatype, int root, MPI_Comm comm) {
+  int nprocs, rank;
+  int tag = 999;
+
+  MPI_Comm_size(comm, &nprocs);
+  MPI_Comm_rank(comm, &rank);
+  if (rank == root) {
+    for (int i = 0; i < nprocs; i++)
+      if (i != root)
+	MPI_Send(buf, count, datatype, i, tag, comm);
+  } else
+    MPI_Recv(buf, count, datatype, root, tag, comm,
+	     MPI_STATUS_IGNORE);
+  return 0;
+}
