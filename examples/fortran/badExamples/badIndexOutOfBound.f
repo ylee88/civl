3e4df15a4ed3823337eac241447a866a53383fe0
Wenhao Wu <wuwenhao@udel.edu>
2016-08-17 18:14:54 +0000

    fix the format of BadIndex Fortran example
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3650 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/fortran/badExamples/badIndexOutOfBound.f
+++ examples/fortran/badExamples/badIndexOutOfBound.f
@@ -2,8 +2,6 @@
       INTEGER N
       PARAMETER (N=10)
       REAL ARRAY(N) !The index of ARRAY should from 1 to 10.
-	ARRAY(11) = 1.0 !It uses an index out of the bound.
+      ARRAY(11) = 1.0 !It uses an index out of the bound.
       END
-      
-
       
