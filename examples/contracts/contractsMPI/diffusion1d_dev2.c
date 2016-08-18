3f4612f2f08b375ae7f03943199e034706e7f073
Ziqing Luo <ziqing@udel.edu>
2016-08-18 17:56:08 +0000

    add new example
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3660 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/contracts/contractsMPI/diffusion1d_dev2.c
+++ examples/contracts/contractsMPI/diffusion1d_dev2.c
@@ -1,30 +1,35 @@
 #include<mpi.h>
-#include<civlc.cvh>
 int left, right, nxl, nx, rank, nprocs;
 double * u, * u_new, k;
 
 #define OWNER(index) ((nprocs*(index+1)-1)/nx)
 
-#define FIRST(index)  u[index - (OWNER(index)*nx/nprocs)]
+#define LOCAL_OF(index)  u[index - (OWNER(index)*nx/nprocs)]
 
 /*@ \mpi_collective(MPI_COMM_WORLD, P2P):
   @   requires rank == \mpi_comm_rank;
   @   requires nxl > 0 && nxl < 5;         //nxl shall not equal to zero
   @   requires \mpi_valid(u, nxl + 2, MPI_DOUBLE);
-  @   assigns  \mpi_region(u, 1, MPI_DOUBLE), 
-  @            \mpi_region(&u[nxl+1], 1, MPI_DOUBLE);
-  @   ensures  \on(left, u[nxl]) == u[0];             
-  @   ensures  \on(right, u[1]) == u[nxl + 1];        
-  @   waitsfor left, right;
   @   behavior maxrank:
   @     assumes rank == \mpi_comm_size - 1;
-  @     requires right == 0 && left == rank - 1;
+  @     requires right == MPI_PROC_NULL && left == rank - 1;
+  @     assigns  \mpi_region(u, 1, MPI_DOUBLE);
+  @     ensures  \on(left, u[nxl]) == u[0];       
+  @     waitsfor left;
   @   behavior minrank:
   @     assumes rank == 0;
-  @     requires left == \mpi_comm_size - 1 && right == rank + 1;
+  @     requires left == MPI_PROC_NULL && right == rank + 1;
+  @     assigns  \mpi_region(&u[nxl+1], 1, MPI_DOUBLE);
+  @     ensures  \on(right, u[1]) == u[nxl + 1];        
+  @     waitsfor right;
   @   behavior others:
   @     assumes 0 < rank && rank < \mpi_comm_size - 1;
   @     requires left == rank - 1 && right == rank + 1;
+  @     assigns  \mpi_region(u, 1, MPI_DOUBLE), 
+  @              \mpi_region(&u[nxl+1], 1, MPI_DOUBLE);
+  @     ensures  \on(left, u[nxl]) == u[0];             
+  @     ensures  \on(right, u[1]) == u[nxl + 1];        
+  @     waitsfor left, right;      
   @*/
 void exchange_ghost_cells() {
   MPI_Sendrecv(&u[1], 1, MPI_DOUBLE, left, 0,
@@ -54,29 +59,28 @@ void update() {
   @ \mpi_collective(MPI_COMM_WORLD, P2P):
   @   requires rank == \mpi_comm_rank;
   @   requires nprocs == \mpi_comm_size;
-  @   requires nxl > 0 && nxl < 5 &&  5 < nx && nx > nxl;
+  @   requires nxl > 0 && nxl < 5;
+  @   requires nx > 5 && nx < 10;
   @   requires \mpi_valid(u, nxl + 2, MPI_DOUBLE);
   @   requires \mpi_valid(u_new, nxl + 2, MPI_DOUBLE);
   @   requires  nx == \sum(0, \mpi_comm_size - 1, 
   @                    (\lambda int k; \on(k, nxl)));
   @   requires k > 0.0;
   @   requires \mpi_agree(nx) && \mpi_agree(k);
-  @   ensures  \forall int i; 0 < i && i <= nx 
-  @            ==>
-  @            \on(OWNER(i), FIRST(i)) == 
-  @            \old( \on(OWNER(i), FIRST(i)) + k* (\on(OWNER(i+1), FIRST(i+1)) + \on(OWNER(i-1), FIRST(i-1)) - 2*\on(OWNER(i), FIRST(i))) ); 
+  @   ensures  \forall int i; 0 < i && i < nxl + 1 ==>
+  @            \on(OWNER(i), LOCAL_OF(i)) != 
+  @            \old( \on(OWNER(i), LOCAL_OF(i)) ); 
   @   behavior maxrank:
   @     assumes rank == \mpi_comm_size - 1;
-  @     requires right == 0 && left == rank - 1;
+  @     requires right == MPI_PROC_NULL && left == rank - 1;
   @   behavior minrank:
   @     assumes rank == 0;
-  @     requires left == \mpi_comm_size - 1 && right == 1;
+  @     requires left == MPI_PROC_NULL && right == 1;
   @   behavior others:
   @     assumes 0 < rank && rank < \mpi_comm_size - 1;
   @     requires left == rank - 1 && right == rank + 1;
   @*/
 void diff1dIter() {
-  $elaborate(nxl);
-  exchange_ghost_cells();
-  update();
+    exchange_ghost_cells();
+    update();
 }
