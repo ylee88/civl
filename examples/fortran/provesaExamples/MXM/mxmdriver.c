ef56832cfdd1c6c9a7a5045c045bbdd3a6e972a6
Wenhao Wu <wuwenhao@udel.edu>
2016-10-20 21:57:57 +0000

    Add a mxm driver file written in C for CIVL.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3743 fb995dde-84ed-4084-dfe6-e5aef3e2452c

---
+++ examples/fortran/provesaExamples/MXM/mxmdriver.c
@@ -0,0 +1,34 @@
+#include<civlc.cvh>
+#include<stdio.h>
+
+/* 
+ * A MXM driver written in C for CIVL.
+ *
+ * ! Notice that arguments of the function declaration MXM44_0
+ * contains two-dimensional array without length info. 
+ * This feature is illegal and invalid for the C standard, 
+ * but it can be handled by CIVL.
+ */
+void MXM44_0(double a[][], int n1, double b[][], int n2, double c[][], int n3);
+
+$input int mSize;
+$assume(0 < mSize && mSize < 11);
+
+void main(){
+    int n = mSize;
+    double a[n][n], b[n][n], c[n][n];
+    
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++){
+            a[i][j] = 1.0 * (i + j);
+            b[i][j] = 1.0 * (i - j);
+            c[i][j] = 0.0;
+        }
+    
+    MXM44_0(a,n,b,n,c,n);
+    
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            printf("c[%d][%d]: %d", i, j, c[i][j]);
+}
+
