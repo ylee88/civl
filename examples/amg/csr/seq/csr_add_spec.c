f96f268b7f24aecf724943a94b48aaddeb3deaa7
Wenhao Wu <wuwenhao@udel.edu>
2016-11-08 22:54:40 +0000

    Add TASS invariant into csr add examples with the Note
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3757 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/amg/csr/seq/csr_add_spec.c
+++ examples/amg/csr/seq/csr_add_spec.c
@@ -3,7 +3,7 @@
 /* = = = = = = = = TASS I/O = = = = = = = = */
 #pragma TASS input {N==1}
 int N;
-#pragma TASS input {M==4}
+#pragma TASS input {M==3}
 int M;
 
 #pragma TASS input
@@ -15,64 +15,74 @@ double OUTS[N*M];
 
 /* = = = = = = = = Dense Matrix Def = = = = = = = = */
 struct DM_struct{
-  double * data;
-  int num_rows;
-  int num_cols;
+    double * data;
+    int num_rows;
+    int num_cols;
 };
 
 typedef struct DM_struct DenseMatrix;
 
 DenseMatrix * dense_create(int n, int m) {
-  DenseMatrix * mat;
+    DenseMatrix * mat;
     
-  mat = (DenseMatrix *) malloc (sizeof(DenseMatrix));
-  mat->num_rows = n;
-  mat->num_cols = m;
-  mat->data = (double*) malloc(n*m*sizeof(double));
-  
-  return mat;
+    mat = (DenseMatrix *) malloc (sizeof(DenseMatrix));
+    mat->num_rows = n;
+    mat->num_cols = m;
+    mat->data = (double*) malloc(n*m*sizeof(double));
+    
+    return mat;
 }
 
-DenseMatrix * dense_add(DenseMatrix * X, DenseMatrix * Y) {
-  int i;
-  int j;
-  int n;
-  int m;
-  DenseMatrix * R;
-
-  n = X->num_rows;
-  m = X->num_cols;
-  R = dense_create(n,m);
-  
-addLoop:
-  for (i=0; i<n; i++)
-    for (j=0; j<m; j++)
-      R->data[i*m + j] = X->data[i*m + j] + Y->data[i*m + j];
-  return R;
+DenseMatrix * hypre_CSRMatrixAdd(DenseMatrix * A, DenseMatrix * B) {
+    int i;
+    int j;
+    int nr;
+    int nc;
+    double * A_data;
+    double * B_data;
+    double * C_data;
+    DenseMatrix * C;
+    
+    nr = A->num_rows;
+    nc = B->num_cols;
+    A_data = A->data;
+    B_data = B->data;
+    C = dense_create(nr,nc);
+    C_data = C->data;
+    
+#pragma TASS joint invariant LoopCondEquiv 0 <= i && i < nr && 0 <= j && j < nc;
+#pragma TASS joint invariant LoopMatAEquiv true;
+#pragma TASS joint invariant LoopMatBEquiv true;
+#pragma TASS joint invariant LoopMatCEquiv true;
+#pragma TASS joint invariant LoopCorrect true;
+    for (i=0; i<nr; i++)
+        for (j=0; j<nc; j++)
+            C_data[i*nc + j] = A_data[i*nc + j] +B_data[i*nc + j];
+    return C;
 }
 
 void free_dense(DenseMatrix * matrix) {
-  free(matrix->data);
-  free(matrix);
+    free(matrix->data);
+    free(matrix);
 }
 
 int main() {
-  int i;
-  DenseMatrix * X;
-  DenseMatrix * Y;
-  DenseMatrix * Z;
-  
-  X = dense_create(N,M);
-  Y = dense_create(N,M);
-  for (i=0; i<N*M; i++)
-    X->data[i] = A0[i];
-  for (i=0; i<N*M; i++)
-    Y->data[i] = B0[i];
-  Z = dense_add(X,Y);
-  for (i=0; i<N*M; i++)
-    OUTS[i] = Z->data[i];
-  free_dense(X);
-  free_dense(Y);
-  free_dense(Z);
-  return 0;
+    int i;
+    DenseMatrix * X;
+    DenseMatrix * Y;
+    DenseMatrix * Z;
+    
+    X = dense_create(N,M);
+    Y = dense_create(N,M);
+    for (i=0; i<N*M; i++)
+        X->data[i] = A0[i];
+    for (i=0; i<N*M; i++)
+        Y->data[i] = B0[i];
+    Z = hypre_CSRMatrixAdd(X,Y);
+    for (i=0; i<N*M; i++)
+        OUTS[i] = Z->data[i];
+    free_dense(X);
+    free_dense(Y);
+    free_dense(Z);
+    return 0;
 }
