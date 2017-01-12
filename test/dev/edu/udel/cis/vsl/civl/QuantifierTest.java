cabd2160a5ba4d92b85ead5569d415f8dd36f2ac
Ziqing Luo <ziqing@udel.edu>
2017-01-12 14:35:08 +0000

    add a simple test for debugging a bug in SARL which misses bound variable declaration when making prover queries
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@3877 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- test/dev/edu/udel/cis/vsl/civl/QuantifierTest.java
+++ test/dev/edu/udel/cis/vsl/civl/QuantifierTest.java
@@ -1,6 +1,5 @@
 package edu.udel.cis.vsl.civl;
 
-
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -10,16 +9,22 @@ import org.junit.Test;
 import edu.udel.cis.vsl.civl.run.IF.UserInterface;
 
 public class QuantifierTest {
-	private static File rootDir = new File(new File("examples"), "experimental");
-	
+	private static File rootDir = new File(new File("examples"),
+			"experimental");
+
 	private static UserInterface ui = new UserInterface();
-	
+
 	private static String filename(String name) {
 		return new File(rootDir, name).getPath();
 	}
-	
+
 	@Test
 	public void quantifier() {
 		assertTrue(ui.run("verify", "-showQueries", filename("quantifier.c")));
+	}
+
+	@Test
+	public void quantifierSARLBug() {
+		assertTrue(ui.run("verify", filename("quantifierSARLBug.cvl")));
 	}
 }
