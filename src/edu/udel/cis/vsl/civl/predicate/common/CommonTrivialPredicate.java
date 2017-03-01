6a2ee2310bd21b7c20677eb288088cc760b9305a
Ziqing Luo <ziqing@udel.edu>
2017-03-01 16:37:28 +0000

    formated with 2.1 built-in formatter
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@4013 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/predicate/common/CommonTrivialPredicate.java
+++ src/edu/udel/cis/vsl/civl/predicate/common/CommonTrivialPredicate.java
@@ -3,8 +3,9 @@ package edu.udel.cis.vsl.civl.predicate.common;
 import edu.udel.cis.vsl.civl.predicate.IF.TrivialPredicate;
 import edu.udel.cis.vsl.civl.state.IF.State;
 
-public class CommonTrivialPredicate extends CommonCIVLStatePredicate implements
-		TrivialPredicate {
+public class CommonTrivialPredicate extends CommonCIVLStatePredicate
+		implements
+			TrivialPredicate {
 
 	@Override
 	public boolean holdsAt(State state) {
