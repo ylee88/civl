901516c79e65fdbb287807f58304843bb8761748
Stephen Siegel <siegel@udel.edu>
2017-05-24 18:03:45 +0000

    Removal of extraneous TODO that no longer apply
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@4286 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/dynamic/common/CommonSymbolicUtility.java
+++ src/edu/udel/cis/vsl/civl/dynamic/common/CommonSymbolicUtility.java
@@ -537,7 +537,6 @@ public class CommonSymbolicUtility implements SymbolicUtility {
 			return this.falseValue;
 		offset = numRefs2 - numRefs1;
 		for (int i = offset; i < numRefs1; i++) {
-			// TODO change to andTo
 			result = universe.and(result, universe.equals(refComps1.get(i),
 					refComps2.get(i + offset)));
 		}
@@ -695,7 +694,6 @@ public class CommonSymbolicUtility implements SymbolicUtility {
 				(NumericExpression) step);
 		BooleanExpression negativeStep = universe
 				.lessThan((NumericExpression) step, zero);
-		// TODO change to andTo
 		BooleanExpression positiveStepResult = universe.and(positiveStep,
 				universe.lessThanEquals((NumericExpression) value,
 						(NumericExpression) high));
