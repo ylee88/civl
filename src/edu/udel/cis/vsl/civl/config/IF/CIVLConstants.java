b57b81e325fe8b81b8f35936de070dc4e7b9ddaf
Ziqing Luo <ziqing@udel.edu>
2017-06-27 20:59:41 +0000

    change version number from 1.10 to 1.10+
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@4389 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
+++ src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
@@ -56,7 +56,7 @@ public class CIVLConstants {
 			new File(File.separator + "include"), "civl");
 
 	/** The version of this release of CIVL. */
-	public final static String version = "1.10";
+	public final static String version = "1.10+";
 
 	/**
 	 * The date of this release of CIVL. Format: YYYY-MM-DD in accordance with
