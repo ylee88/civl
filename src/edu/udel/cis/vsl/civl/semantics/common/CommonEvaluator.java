62b13a052100063ccefa72c052da4d6383370f6f
Ziqing Luo <ziqing@udel.edu>
2017-08-14 16:57:09 +0000

    Improve the source associated to DIVISION_BY_ZERO error
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@4473 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/semantics/common/CommonEvaluator.java
+++ src/edu/udel/cis/vsl/civl/semantics/common/CommonEvaluator.java
@@ -449,10 +449,10 @@ public class CommonEvaluator implements Evaluator {
 	 *            false only when executing $copy function.
 	 * @param muteErrorSideEffects
 	 *            Should this method mute error side-effects ? i.e.
-	 *            Dereferencing a pointer with error side-effects
-	 *            <strong> results an undefined value of the same type as the
-	 *            dereference expression </strong> iff this parameter set to
-	 *            true. Otherwise, an error will be reported and
+	 *            Dereferencing a pointer with error side-effects <strong>
+	 *            results an undefined value of the same type as the dereference
+	 *            expression </strong> iff this parameter set to true.
+	 *            Otherwise, an error will be reported and
 	 *            UnsatisfiablePathConditionException will be thrown.
 	 * @return A possibly new state and the value of memory space pointed by the
 	 *         pointer.
@@ -493,13 +493,11 @@ public class CommonEvaluator implements Evaluator {
 							.variable(vid);
 
 					if (variable.isOutput()) {
-						errorLogger
-								.logSimpleError(source, state, process,
-										symbolicAnalyzer.stateInformation(
-												state),
-										ErrorKind.OUTPUT_READ,
-										"Attempt to read output variable "
-												+ variable.name().name());
+						errorLogger.logSimpleError(source, state, process,
+								symbolicAnalyzer.stateInformation(state),
+								ErrorKind.OUTPUT_READ,
+								"Attempt to read output variable "
+										+ variable.name().name());
 						throwPCException = true;
 					}
 				}
@@ -554,10 +552,10 @@ public class CommonEvaluator implements Evaluator {
 	 *            The pointer to be dereferenced.
 	 * @param muteErrorSideEffects
 	 *            Should this method mute error side-effects ? i.e.
-	 *            Dereferencing a pointer with error side-effects
-	 *            <strong> results an undefined value of the same type as the
-	 *            dereference expression </strong> iff this parameter set to
-	 *            true. Otherwise, an error will be reported and
+	 *            Dereferencing a pointer with error side-effects <strong>
+	 *            results an undefined value of the same type as the dereference
+	 *            expression </strong> iff this parameter set to true.
+	 *            Otherwise, an error will be reported and
 	 *            UnsatisfiablePathConditionException will be thrown.
 	 * @param source
 	 *            The {@link CIVLSource} associates with the dereference
@@ -1898,9 +1896,9 @@ public class CommonEvaluator implements Evaluator {
 					.getResultType();
 
 			if (resultType != ResultType.YES)
-				state = errorLogger.logError(expression.getSource(), state, pid,
-						symbolicAnalyzer.stateInformation(state), claim,
-						resultType, ErrorKind.DIVISION_BY_ZERO,
+				state = errorLogger.logError(expression.right().getSource(),
+						state, pid, symbolicAnalyzer.stateInformation(state),
+						claim, resultType, ErrorKind.DIVISION_BY_ZERO,
 						"Modulus denominator is zero");
 		}
 		return new Evaluation(state, universe.modulo(numerator, denominator));
@@ -1974,7 +1972,8 @@ public class CommonEvaluator implements Evaluator {
 			if (resultType != ResultType.YES) {
 				Expression divisor = expression.right();
 
-				state = errorLogger.logError(expression.getSource(), state, pid,
+				state = errorLogger.logError(expression.right().getSource(),
+						state, pid,
 						this.symbolicAnalyzer.stateInformation(state), claim,
 						resultType, ErrorKind.DIVISION_BY_ZERO,
 						"division by zero where divisor: " + expression.right()
@@ -3939,13 +3938,11 @@ public class CommonEvaluator implements Evaluator {
 				state = eval.state;
 				// A single character is not acceptable.
 				if (eval.value.numArguments() <= 1) {
-					this.errorLogger
-							.logSimpleError(source, state, process,
-									this.symbolicAnalyzer.stateInformation(
-											state),
-									ErrorKind.OTHER,
-									"Try to obtain a string from a sequence of char has length"
-											+ " less than or equal to one");
+					this.errorLogger.logSimpleError(source, state, process,
+							this.symbolicAnalyzer.stateInformation(state),
+							ErrorKind.OTHER,
+							"Try to obtain a string from a sequence of char has length"
+									+ " less than or equal to one");
 					throw new UnsatisfiablePathConditionException();
 				} else {
 					originalArray = eval.value;
