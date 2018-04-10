f98a01794e78ed6e3fbafc9eab23b0e4c556caba
Ziqing Luo <ziqing@udel.edu>
2018-04-10 15:40:24 +0000

    commit before minor release
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@4743 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
+++ src/edu/udel/cis/vsl/civl/config/IF/CIVLConstants.java
@@ -56,13 +56,13 @@ public class CIVLConstants {
 			new File(File.separator + "include"), "civl");
 
 	/** The version of this release of CIVL. */
-	public final static String version = "1.15+";
+	public final static String version = "1.15.1";
 
 	/**
 	 * The date of this release of CIVL. Format: YYYY-MM-DD in accordance with
 	 * ISO 8601.
 	 */
-	public final static String date = "2018-04-06";
+	public final static String date = "2018-04-10";
 
 	/**
 	 * The prefix of the full name of the class of a library enabler/executor.
