e84ea011f3f89bf8dd2e799092ceac8cc8279608
Alex Wilton <awilton@udel.edu>
2021-06-29 16:51:35 +0000

    Forgot to add the kernel's actual definition to the bug example.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@5414 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- examples/cuda/kernel_after_main.cu
+++ examples/cuda/kernel_after_main.cu
@@ -30,3 +30,11 @@ int main ()
   cudaFree(d_data);
   return 0;
 }
+
+__global__ void test_kernel(int* data, int numv)
+{
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  if (idx < numv) {
+    data[0]++;
+  }
+}
