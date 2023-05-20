c9726395c1fa93470e2b0203dad7134fb8ca4a17
Stephen Siegel <siegel@udel.edu>
2023-05-20 20:33:49 +0000

    Removing unneeded material, copying to civl-extras repo.
    git-svn-id: svn://vsl.cis.udel.edu/civl/trunk@5770 fb995dde-84ed-4084-dfe6-e5aef3e2452c

--- mods/dev.civl.com/doc/manual/sumlib/sumlib_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <assert.h>
-#include "sumlib.h"
-#define N 100
-int main() {
-  int result = sum(N);
-  printf("N=%d, sum = %d\n", N, result);
-  assert(result == (N+1)*N/2);
-}
